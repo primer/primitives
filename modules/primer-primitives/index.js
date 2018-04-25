39638891fb0b8d97e962db4728eba24a0e406ca4
Diana Mounter <broccolini@github.com>
2018-04-25 15:42:52 -0400

    simplify consts

--- modules/primer-primitives/index.js
+++ modules/primer-primitives/index.js
@@ -3,10 +3,10 @@ const space = require('primer-spacing')
 const { fontSizes, lineHeights } = require('primer-typography')
 
 const theme = {
-  colors: colors,
-  space: space,
-  fontSizes: fontSizes,
-  lineHeights: lineHeights
+  colors,
+  space,
+  fontSizes,
+  lineHeights
 }
 
 module.exports = theme
