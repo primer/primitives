60aec3b0cf7005634d351cad058e3180864b683f
Diana Mounter <broccolini@github.com>
2018-04-24 21:09:24 -0400

    add combined primitives package

---
+++ modules/primer-primitives/index.js
@@ -0,0 +1,10 @@
+const colors = require('primer-colors')
+const space = require('primer-spacing')
+const { fontSizes, lineHeights } = require('primer-typography')
+
+const theme = {
+  colors: colors,
+  space: space,
+  fontSizes: fontSizes,
+  lineHeights: lineHeights
+}
