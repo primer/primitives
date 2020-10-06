109dae0e1216f67a63ca567e006cf0e46bc475bd
Cole Bemis <colebemis@github.com>
2020-10-06 13:32:55 -0700

    Remove template comments

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,10 +1,3 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
   plugins: ['gatsby-plugin-styled-components']
 }
