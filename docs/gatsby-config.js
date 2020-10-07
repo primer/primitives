289de1a2289a54a629733f72685e23a0215c6d09
Cole Bemis <colebemis@github.com>
2020-10-07 12:42:56 -0700

    Add favicon

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: ['gatsby-plugin-styled-components', 'gatsby-plugin-react-helmet']
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: require.resolve('./src/images/favicon.png')
+      }
+    }
+  ]
 }
