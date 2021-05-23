7d4168e3e1a314338113c491f7a29d2ce8bf827e
Cole Bemis <colebemis@github.com>
2021-05-23 18:01:08 -0500

    Convert typography to TS

---
+++ data/typography/index.ts
@@ -0,0 +1,10 @@
+export default {
+  normal: {
+    fontSize: ['12px', '14px', '16px', '20px', '24px', '32px', '40px', '48px'],
+    lineHeight: {
+      condensedUltra: 1,
+      condensed: 1.25,
+      default: 1.5
+    }
+  }
+}
