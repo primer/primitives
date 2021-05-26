b7f94e8cf9c6cf1a94318af9feaf40cb14ac5a4a
Cole Bemis <colebemis@github.com>
2021-05-26 10:51:49 -0700

    Reorganize file structure

--- data/colors_v2/index.ts
+++ data/colors_v2/index.ts
@@ -1,7 +1,7 @@
-import light from './light'
-import dark from './dark'
-import dark_dimmed from './dark_dimmed'
-import dark_high_contrast from './dark_high_contrast'
+import light from './themes/light'
+import dark from './themes/dark'
+import dark_dimmed from './themes/dark_dimmed'
+import dark_high_contrast from './themes/dark_high_contrast'
 
 export default {
   light,
