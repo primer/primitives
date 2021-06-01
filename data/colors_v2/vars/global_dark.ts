670db943308b26bb17aef72c03066e5725073e73
Aurora Pleguezuelo <auareyou@github.com>
2021-06-01 14:29:00 +0200

    Updated .`subtle` to 0.15

--- data/colors_v2/vars/global_dark.ts
+++ data/colors_v2/vars/global_dark.ts
@@ -34,43 +34,43 @@ export default {
     fg: get('scale.blue.3'),
     emphasis: get('scale.blue.5'),
     muted: alpha(get('scale.blue.4'), 0.4),
-    subtle: alpha(get('scale.blue.4'), 0.1)
+    subtle: alpha(get('scale.blue.4'), 0.15)
   },
   success: {
     fg: get('scale.green.3'),
     emphasis: get('scale.green.5'),
     muted: alpha(get('scale.green.4'), 0.4),
-    subtle: alpha(get('scale.green.4'), 0.1)
+    subtle: alpha(get('scale.green.4'), 0.15)
   },
   warning: {
     fg: get('scale.yellow.3'),
     emphasis: get('scale.yellow.5'),
     muted: alpha(get('scale.yellow.4'), 0.4),
-    subtle: alpha(get('scale.yellow.4'), 0.1)
+    subtle: alpha(get('scale.yellow.4'), 0.15)
   },
   severe: {
     fg: get('scale.orange.4'),
     emphasis: get('scale.orange.5'),
     muted: alpha(get('scale.orange.4'), 0.4),
-    subtle: alpha(get('scale.orange.4'), 0.1)
+    subtle: alpha(get('scale.orange.4'), 0.15)
   },
   danger: {
     fg: get('scale.red.4'),
     emphasis: get('scale.red.5'),
     muted: alpha(get('scale.red.4'), 0.4),
-    subtle: alpha(get('scale.red.4'), 0.1)
+    subtle: alpha(get('scale.red.4'), 0.15)
   },
   done: {
     fg: get('scale.purple.4'),
     emphasis: get('scale.purple.5'),
     muted: alpha(get('scale.purple.4'), 0.4),
-    subtle: alpha(get('scale.purple.4'), 0.1)
+    subtle: alpha(get('scale.purple.4'), 0.15)
   },
   sponsors: {
     fg: get('scale.pink.4'),
     emphasis: get('scale.pink.5'),
     muted: alpha(get('scale.pink.4'), 0.4),
-    subtle: alpha(get('scale.pink.4'), 0.1)
+    subtle: alpha(get('scale.pink.4'), 0.15)
   },
 
   // Only meant for Primer components
