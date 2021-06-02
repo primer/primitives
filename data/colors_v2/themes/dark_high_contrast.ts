28dd756ea0ced0c582607b99d17012781bff961e
Julius Yanik <juliusschaeper@github.com>
2021-06-02 17:56:41 +0200

    Updated 'Light' and 'Dark High Contrast' gray scales

--- data/colors_v2/themes/dark_high_contrast.ts
+++ data/colors_v2/themes/dark_high_contrast.ts
@@ -4,7 +4,7 @@ import dark from './dark'
 const scale = {
   black: '#010409',
   white: '#ffffff',
-  gray: ['#ffffff', '#f1f3f6', '#d7dbdf', '#b9bfc6', '#98a1ad', '#79828e', '#5b616b', '#272b33', '#272b33', '#090c11'],
+  gray: ['#ffffff', '#f0f3f6', '#d9dee3', '#bdc4cc', '#9ea7b3', '#7a828e', '#525964', '#272b33', '#272b33', '#00a0c10'],
   blue: ['#cce9ff', '#aedcff', '#8eccff', '#6cb8ff', '#589df3', '#1f56aa', '#0d4091', '#0c306d', '#072357', '#041844'],
   green: ['#bcf3bc', '#88ec90', '#63de70', '#4bc95e', '#35b14d', '#1d672e', '#134f23', '#083c17', '#022d0f', '#03200c'],
   yellow: [
