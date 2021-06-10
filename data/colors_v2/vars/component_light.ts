75c3b1830dc80df54ac283208f1eb8572ae9b906
Aurora Pleguezuelo <auareyou@github.com>
2021-06-10 15:42:55 +0200

    Errors

--- data/colors_v2/vars/component_light.ts
+++ data/colors_v2/vars/component_light.ts
@@ -3,6 +3,7 @@ import {alpha, darken, get} from '../../../src/utils'
 export default {
   avatar: {
     bg: get('scale.white'),
+    border: get('border.subtle'),
     stackFade: get('scale.gray.3'),
     stackFadeMore: get('scale.gray.2'),
     childShadow: (theme: any) => `-2px -2px 0 ${alpha(get('scale.white'), 0.8)(theme)}`
