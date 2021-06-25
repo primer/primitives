783049b35ceef0113e0c6325c9add8304ee9b9f7
simurai <simurai@github.com>
2021-06-25 16:59:18 +0900

    Deprecate text-white

--- data/colors_v2/vars/deprecated.ts
+++ data/colors_v2/vars/deprecated.ts
@@ -120,7 +120,7 @@ export default {
     danger: get('danger.fg'),
     success: get('success.fg'),
     warning: get('attention.fg'),
-    white: get('fg.onEmphasis')
+    white: deprecated,
   },
   icon: {
     primary: get('fg.default'),
