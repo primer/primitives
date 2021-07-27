5e5f7ea33741f99a4a230ce20d9fb4c4f3b2772d
Julius Yanik <juliusschaeper@github.com>
2021-07-27 12:34:26 +0200

    Underline nav contrast improvements
    Based on this feedback I https://github.com/github/github/discussions/183946#discussioncomment-1051866
    
    - Unified icon color
    - Increased border hover contrast which was reported as well as an issue.
    - Mapped coral to scale

--- data/colors/dark.ts
+++ data/colors/dark.ts
@@ -601,7 +601,7 @@ export default {
   underlinenav: {
     border: alpha(get('scale.gray.6'), 0),
     borderHover: get('scale.gray.6'),
-    borderActive: '#f78166',
+    borderActive: get('scale.coral.3'),
     text: get('scale.gray.3'),
     textHover: get('scale.gray.1'),
     textActive: get('scale.gray.1'),
