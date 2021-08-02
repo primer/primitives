f7295e468ec84adc21883766b5fad592edb41dfa
Aurora Pleguezuelo <auareyou@github.com>
2021-08-02 20:14:22 +0200

    Removed repo header commit

--- data/colors_v2/themes/dark.ts
+++ data/colors_v2/themes/dark.ts
@@ -55,11 +55,6 @@ const exceptions = {
   fg: {
     fileIcon: get('fg.muted')
   },
-  canvas: {
-    repoHeader: get('canvas.default')
-  },
-
-  
 }
 
 export default merge(deprecatedVars, darkProductVars, darkMarketingVars, darkComponentVars, darkGlobalVars, {scale})
