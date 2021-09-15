52a27e7214588b4f1d1745b09503ba67609589f2
Julius Yanik <juliusschaeper@github.com>
2021-09-15 14:37:37 +0200

    Changed colorblind theme names
    Colorblind wasn’t appropriate so I changed it to Protanopia. Protanopia stands for Red/Green blindness.

--- data/colors/index.ts
+++ data/colors/index.ts
@@ -1,15 +1,15 @@
 import light from './themes/light'
-import light_color_blind from './themes/light_color_blind'
+import light_protanopia from './themes/light_protanopia'
 import dark from './themes/dark'
 import dark_dimmed from './themes/dark_dimmed'
 import dark_high_contrast from './themes/dark_high_contrast'
-import dark_color_blind from './themes/dark_color_blind'
+import dark_protanopia from './themes/dark_protanopia'
 
 export default {
   light,
-  light_color_blind,
+  light_protanopia,
   dark,
   dark_dimmed,
   dark_high_contrast,
-  dark_color_blind
+  dark_protanopia
 }
