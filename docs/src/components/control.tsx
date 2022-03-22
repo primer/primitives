8accbcfef728eedb1eccb58fefd8edf14a103228
langermank <langermank@github.com>
2022-03-21 20:34:13 -0700

    var table styles in docs

--- docs/src/components/control.tsx
+++ docs/src/components/control.tsx
@@ -6,6 +6,10 @@ import Table from '@primer/gatsby-theme-doctocat/src/components/table.js'
 import tokens from '../../../dist/docs/docValues.json'
 import styled, {createGlobalStyle} from 'styled-components'
 
+export const highlightColorVariants = ['transparent', 'filled'] as const
+
+type highlightColorVariant = typeof highlightColorVariants[number]
+
 interface ControlVisualProps {
   paddingLeft?: string
   paddingRight?: string
@@ -15,6 +19,16 @@ interface ControlVisualProps {
   color?: string
   blockSize?: string
   lineBox?: string
+  showBackgroundColor?: boolean
+  modifier?: string
+  highlightColor?: highlightColorVariant
+  highlightPaddingBottom?: boolean
+  highlightPaddingTop?: boolean
+  highlightPaddingRight?: boolean
+  highlightPaddingLeft?: boolean
+  highlightGap?: boolean
+  highlightLineBoxHeight?: boolean
+  highlightHeight?: boolean
 }
 
 const GlobalStyle = createGlobalStyle`
@@ -33,43 +47,132 @@ const ControlVisual: FC<ControlVisualProps> = ({
   gap,
   color,
   blockSize,
-  lineBox
+  lineBox,
+  modifier = '-condensed',
+  showBackgroundColor,
+  highlightColor = 'transparent',
+  highlightPaddingBottom,
+  highlightPaddingTop,
+  highlightPaddingRight,
+  highlightPaddingLeft,
+  highlightGap,
+  highlightLineBoxHeight,
+  highlightHeight
 }) => {
   return (
     <Fragment>
       <GlobalStyle />
-      <div
-        style={{
+      <Box
+        as="div"
+        sx={{
           display: 'inline-grid',
           gridTemplateAreas: `'pTop pTop pTop pTop pTop' 'pLeft icon gap label pRight' 'pBottom pBottom pBottom pBottom pBottom'`,
           gridTemplateRows: 'min-content minmax(0, 1fr) min-content',
           gridTemplateColumns: 'repeat(5, min-content)',
           borderRadius: 'var(--gh-borderRadius-medium)',
           border: 'solid 1px var(--scale-purple-3)',
-          height: blockSize
+          height: `var(--gh-control-${blockSize}-size)`,
+          backgroundImage:
+            highlightHeight &&
+            'linear-gradient(45deg,#a475f980 12.50%,#d8b9ff7a 12.50%,#d8b9ff7a 50%,#a475f980 50%,#a475f980 62.50%,#d8b9ff7a 62.50%,#d8b9ff7a 100%)',
+          backgroundSize: '5.66px 5.66px',
+          alignItems: 'center'
         }}
       >
-        <span
-          style={{
+        <Box
+          as="span"
+          sx={{
             gridArea: 'pTop',
-            height: paddingTop,
-            backgroundColor: 'var(--scale-purple-3)'
+            height: `var(--gh-control-${paddingTop}-paddingBlock)`,
+            backgroundImage:
+              highlightPaddingTop &&
+              'linear-gradient(45deg,#a475f980 12.50%,#d8b9ff7a 12.50%,#d8b9ff7a 50%,#a475f980 50%,#a475f980 62.50%,#d8b9ff7a 62.50%,#d8b9ff7a 100%)',
+            backgroundSize: '5.66px 5.66px'
           }}
-        ></span>
-        <span style={{gridArea: 'pLeft', width: paddingLeft, backgroundColor: 'var(--scale-pink-3)'}}></span>
-        <span style={{gridArea: 'icon', display: 'flex', alignItems: 'center'}}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+        />
+        <Box
+          as="span"
+          sx={{
+            gridArea: 'pLeft',
+            width: `var(--gh-control-${paddingLeft}-paddingInline${modifier})`,
+            backgroundImage:
+              highlightPaddingLeft &&
+              'linear-gradient(45deg,#a475f980 12.50%,#d8b9ff7a 12.50%,#d8b9ff7a 50%,#a475f980 50%,#a475f980 62.50%,#d8b9ff7a 62.50%,#d8b9ff7a 100%)',
+            backgroundSize: '5.66px 5.66px',
+            height: '100%'
+          }}
+        />
+        <Box
+          as="span"
+          sx={{
+            gridArea: 'icon',
+            display: 'flex',
+            alignItems: 'center'
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            width="16"
+            height="16"
+            aria-hidden="true"
+            focusable="false"
+          >
             <path
               fill-rule="evenodd"
               d="M5.75 4A1.75 1.75 0 004 5.75v4.5c0 .966.784 1.75 1.75 1.75h4.5A1.75 1.75 0 0012 10.25v-4.5A1.75 1.75 0 0010.25 4h-4.5z"
             ></path>
           </svg>
-        </span>
-        <span style={{gridArea: 'gap', width: gap, backgroundColor: 'var(--scale-yellow-1)'}}></span>
-        <span style={{gridArea: 'label', lineHeight: lineBox}}>label</span>
-        <span style={{gridArea: 'pRight', width: paddingRight, backgroundColor: 'var(--scale-pink-3)'}}></span>
-        <span style={{gridArea: 'pBottom', height: paddingBottom, backgroundColor: 'var(--scale-purple-3)'}}></span>
-      </div>
+        </Box>
+        <Box
+          as="span"
+          sx={{
+            gridArea: 'gap',
+            width: `var(--gh-control-${gap}-gap)`,
+            backgroundImage:
+              highlightGap &&
+              'linear-gradient(45deg,#a475f980 12.50%,#d8b9ff7a 12.50%,#d8b9ff7a 50%,#a475f980 50%,#a475f980 62.50%,#d8b9ff7a 62.50%,#d8b9ff7a 100%)',
+            backgroundSize: '5.66px 5.66px'
+          }}
+        />
+        <Box
+          as="span"
+          sx={{
+            gridArea: 'label',
+            lineHeight: `var(--gh-control-${lineBox}-lineBoxHeight)`,
+            backgroundImage:
+              highlightLineBoxHeight &&
+              'linear-gradient(45deg,#a475f980 12.50%,#d8b9ff7a 12.50%,#d8b9ff7a 50%,#a475f980 50%,#a475f980 62.50%,#d8b9ff7a 62.50%,#d8b9ff7a 100%)',
+            backgroundSize: '5.66px 5.66px',
+            fontSize: 'var(--gh-text-body-size-medium)'
+          }}
+        >
+          label
+        </Box>
+        <Box
+          as="span"
+          sx={{
+            gridArea: 'pRight',
+            width: `var(--gh-control-${paddingRight}-paddingInline${modifier})`,
+            backgroundImage:
+              highlightPaddingRight &&
+              'linear-gradient(45deg,#a475f980 12.50%,#d8b9ff7a 12.50%,#d8b9ff7a 50%,#a475f980 50%,#a475f980 62.50%,#d8b9ff7a 62.50%,#d8b9ff7a 100%)',
+            backgroundSize: '5.66px 5.66px',
+            height: '100%'
+          }}
+        />
+        <Box
+          as="span"
+          sx={{
+            gridArea: 'pBottom',
+            height: `var(--gh-control-${paddingBottom}-paddingBlock)`,
+            backgroundImage:
+              highlightPaddingBottom &&
+              'linear-gradient(45deg,#a475f980 12.50%,#d8b9ff7a 12.50%,#d8b9ff7a 50%,#a475f980 50%,#a475f980 62.50%,#d8b9ff7a 62.50%,#d8b9ff7a 100%)',
+            backgroundSize: '5.66px 5.66px'
+          }}
+        />
+      </Box>
     </Fragment>
   )
 }
--- docs/src/components/token-block.tsx
+++ docs/src/components/token-block.tsx
@@ -12,35 +12,21 @@ import ControlVisual from './control'
 const GlobalStyle = createGlobalStyle`
   code {
       white-space: nowrap;
+        background: var(--scale-purple-1);
+        border-radius: 6px;
+        padding: 2px 6px;
+        flex: 0 1 auto;
+  }
+  :root {
+      --scale-purple-1: #ecd8ff;
+    --scale-purple-3: #c297ff;
+    --scale-pink-3: #8250df;
+    --scale-yellow-1: #6639ba;
+    --color-fg-subtle: #6e7781;
   }
 `
 
-// export function TokenBlockVisual({filePath}) {
-//   return tokens[filePath].map(token => {
-//     console.log(token.name.includes('gap'))
-//     const gapProp = token.name.includes('gap') ? token.value : null
-//     const paddingLeft = token.name.includes('paddingInline') ? token.value : null
-//     const paddingRight = token.name.includes('paddingInline') ? token.value : null
-//     const paddingTop = token.name.includes('paddingBlock') ? token.value : null
-//     const paddingBottom = token.name.includes('paddingBlock') ? token.value : null
-//       const blockSize = token.name.includes('size') ? token.value : null
-//   )
-//     return (
-//       <ControlVisual
-//         gap={gapProp}
-//         paddingLeft={paddingLeft}
-//         paddingRight={paddingRight}
-//         paddingTop={paddingTop}
-//         paddingBottom={paddingBottom}
-//         blockSize={blockSize}
-//       />
-//     )
-//   })
-// }
-
-export function TokenBlock({filePath}) {
-  // return <pre>{JSON.stringify(tokens[filePath], null, 2)}</pre>
-  //   console.log(tokens)
+export function TokenBlock({filePath, tokenVariant}) {
   return (
     <Table>
       <GlobalStyle />
@@ -48,29 +34,106 @@ export function TokenBlock({filePath}) {
         <thead>
           <tr>
             <Box as="th" textAlign="left">
-              CSS
+              Example
             </Box>
             <Box as="th" textAlign="left">
-              JS
+              Token
             </Box>
             <Box as="th" textAlign="left">
               Value
+            </Box>
+            <Box as="th" textAlign="left">
+              px
             </Box>
           </tr>
         </thead>
         <tbody>
           {tokens[filePath].map(token => {
+            const gapProp = token.name.includes('gap') ? true : false
+            const paddingLeft = token.name.includes('paddingInline') ? true : false
+            const paddingRight = token.name.includes('paddingInline') ? true : false
+            const paddingTop = token.name.includes('paddingBlock') ? true : false
+            const paddingBottom = token.name.includes('paddingBlock') ? true : false
+            const lineBlockHeightProp = token.name.includes('lineBoxHeight') ? true : false
+            const blockSize = token.name.includes('size') ? true : false
+            const condensed = token.name.includes('condensed') ? true : false
+            const normal = token.name.includes('normal') ? true : false
+            const spacious = token.name.includes('spacious') ? true : false
+            const tokenVariantString = tokenVariant.replace(/-/g, '')
+            console.log(tokenVariant)
             return (
-              token.name.includes('-small-') && (
+              token.name.includes('-control-') &&
+              token.name.match(tokenVariant) && (
                 <tr>
                   <td>
-                    <code>--{token.name}</code>
+                    <ControlVisual
+                      gap={tokenVariantString}
+                      paddingLeft={tokenVariantString}
+                      paddingRight={tokenVariantString}
+                      paddingTop={tokenVariantString}
+                      paddingBottom={tokenVariantString}
+                      blockSize={tokenVariantString}
+                      lineBox={tokenVariantString}
+                      modifier={condensed ? '-condensed' : normal ? '-normal' : spacious ? '-spacious' : undefined}
+                      highlightPaddingBottom={paddingBottom}
+                      highlightPaddingTop={paddingTop}
+                      highlightPaddingRight={paddingRight}
+                      highlightPaddingLeft={paddingLeft}
+                      highlightGap={gapProp}
+                      highlightHeight={blockSize}
+                      highlightLineBoxHeight={lineBlockHeightProp}
+                    />
                   </td>
-                  <td>
-                    <code>{token.path.join('.')}</code>
-                  </td>
+
+                  <Box as="td" sx={{padding: '0 !important'}}>
+                    <Box
+                      as="table"
+                      sx={{
+                        '& tr': {
+                          border: 'none !important',
+                          display: 'grid',
+                          gridTemplateColumns: '3ch 2fr',
+                          alignItems: 'center',
+                          justifyItems: 'start',
+                          gap: '16px'
+                        },
+                        '& td': {border: 'none !important', padding: '0 !important', display: 'flex'},
+                        '& th': {
+                          border: 'none !important',
+                          color: 'var(--color-fg-subtle)',
+                          fontSize: '14px',
+                          padding: '0 !important'
+                        },
+                        '& tr:nth-child(2n)': {background: 'transparent !important'},
+                        '& tbody': {
+                          display: 'flex',
+                          flexDirection: 'column',
+                          padding: '8px 16px',
+                          gap: '8px'
+                        }
+                      }}
+                    >
+                      <tbody>
+                        <tr>
+                          <th>CSS</th>
+                          <td>
+                            <code>--{token.name}</code>
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>JS</th>
+                          <td>
+                            <code>{token.path.join('.')}</code>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </Box>
+                  </Box>
                   <td>
                     <code>{token.value}</code>
+                  </td>
+                  <td>
+                    <code>{token.original.value}</code>
                   </td>
                 </tr>
               )
