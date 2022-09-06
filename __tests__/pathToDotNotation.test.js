57461491236762b6655b73fac6440e92ca6d356b
Lukas Oppermann <lukasoppermann@github.com>
2022-09-06 12:15:04 +0200

    Update __tests__/pathToDotNotation.test.js
    Great idea.
    
    Co-authored-by: Rez <rezrah@github.com>

--- __tests__/pathToDotNotation.test.js
+++ __tests__/pathToDotNotation.test.js
@@ -1,6 +1,7 @@
 const pathToDotNotation = require('../build.js').pathToDotNotation
 
-test('valid token', () => {
+describe('Token formatters: PascalCase', () => {
+  it('returns a valid token name in PascalCase', () => {
   expect(pathToDotNotation({path: ['red']})).toBe('red')
   expect(pathToDotNotation({path: ['color', 'red']})).toBe('color.red')
   expect(pathToDotNotation({path: ['color', 'functional', 'danger']})).toBe('color.functional.danger')
