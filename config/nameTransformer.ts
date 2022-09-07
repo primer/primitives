d0d1cb4239c52e6f76adab709535bccd7b428b63
Lukas Oppermann <lukasoppermann@github.com>
2022-09-07 14:44:18 +0200

    use SD type

--- config/nameTransformer.ts
+++ config/nameTransformer.ts
@@ -1,12 +1,12 @@
-import { Token } from '../src/@types/token'
+import { TransformedToken } from 'style-dictionary'
 
-const pathToKebabCase = (token: Token) => token.path.join('-')
+const pathToKebabCase = (token: TransformedToken) => token.path.join('-')
 
-const pathToDotNotation = (token: Token) => token.path.join('.')
+const pathToDotNotation = (token: TransformedToken) => token.path.join('.')
 
 const capitalize = (string: string) => string[0].toUpperCase() + string.slice(1)
 
-const pathToPascalCase = (token: Token) => token.path.map(tokenPathItems => capitalize(tokenPathItems)).join('')
+const pathToPascalCase = (token: TransformedToken) => token.path.map(tokenPathItems => capitalize(tokenPathItems)).join('')
 
 export {
   pathToKebabCase,
