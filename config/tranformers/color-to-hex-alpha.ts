c9027a7d159758d08fbd1a5f218f248581c3976b
Lukas Oppermann <lukasoppermann@github.com>
2022-10-04 10:43:25 +0200

    replace color2k.transparentize with custom alpha function

--- config/tranformers/color-to-hex-alpha.ts
+++ config/tranformers/color-to-hex-alpha.ts
@@ -1,11 +1,11 @@
-import { toHex, transparentize } from 'color2k'
+import { toHex } from 'color2k'
+import { alpha } from '../utilities/alpha'
 import StyleDictionary from 'style-dictionary'
 
 export const colorToHexAlpha: StyleDictionary.Transform = {
   type: `value`,
   transitive: true,
   matcher: (token: StyleDictionary.TransformedToken) => token.$type === 'color' && token.alpha,
-  // color2K transparentize needs a "value to reduce opacity by", normally we have a desired opacity value so we need to do 1 - alpha
   // color2k toHex does output hex8 if opacity is less than 1, it is jsut not documented. :(
-  transformer: (token: StyleDictionary.TransformedToken) => toHex(transparentize(token.value, 1 - token.alpha))
+  transformer: (token: StyleDictionary.TransformedToken) => toHex(alpha(token.value, token.alpha))
 }
---
+++ config/utilities/alpha.ts
@@ -0,0 +1,12 @@
+import { rgba, parseToRgba } from 'color2k'
+/**
+ * alpha
+ * @description takes a colors string like hex or rgba and returns an rgba color with the specified alpha value
+ * @param color color string like a `#334455` or `rgb(255,200,100)`
+ * @param desiredAlpha number
+ * @returns rgba value
+ */
+export const alpha = (color: string, desiredAlpha: number): string => {
+  const [r, g, b] = parseToRgba(color);
+  return rgba(r, g, b, desiredAlpha);
+}
