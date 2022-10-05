a89f7551bc60c17f1175389802efef5e17559d89
Lukas Oppermann <lukasoppermann@github.com>
2022-10-05 14:24:48 +0200

    add error handling for undefined value

--- config/tranformers/color-to-hex-alpha.ts
+++ config/tranformers/color-to-hex-alpha.ts
@@ -6,6 +6,10 @@ export const colorToHexAlpha: StyleDictionary.Transform = {
   type: `value`,
   transitive: true,
   matcher: (token: StyleDictionary.TransformedToken) => token.$type === 'color' && token.alpha !== undefined,
-  // color2k toHex does output hex8 if opacity is less than 1, it is jsut not documented. :(
-  transformer: (token: StyleDictionary.TransformedToken) => toHex(alpha(token.value, token.alpha))
+  // color2k toHex does output hex8 if opacity is less than 1, it is just not documented. :(
+  transformer: (token: StyleDictionary.TransformedToken) => {
+    if (!token.value) throw new Error(`Missing value for token: ${token.path.join('.')} from file "${token.filePath}"`);
+
+    return toHex(alpha(token.value, token.alpha))
+  }
 }
--- config/tranformers/color-to-rgb-alpha.ts
+++ config/tranformers/color-to-rgb-alpha.ts
@@ -5,5 +5,8 @@ export const colorToRgbAlpha: StyleDictionary.Transform = {
   type: `value`,
   transitive: true,
   matcher: (token: StyleDictionary.TransformedToken) => token.$type === 'color' && token.alpha !== undefined,
-  transformer: (token: StyleDictionary.TransformedToken) => alpha(token.value, token.alpha)
+  transformer: (token: StyleDictionary.TransformedToken) => {
+    if (!token.value) throw new Error(`Missing value for token: ${token.path.join('.')} from file "${token.filePath}"`);
+    return alpha(token.value, token.alpha)
+  }
 }
