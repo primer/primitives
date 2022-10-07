fe60528f0d1e79fc4fe31b85e1650e250f9d5f1b
Lukas Oppermann <lukasoppermann@github.com>
2022-10-07 10:33:27 +0200

    remove one @ts-ignore

--- script/utilities/flattenObject.ts
+++ script/utilities/flattenObject.ts
@@ -1,9 +1,20 @@
+/**
+ * flattenObject
+ * @description turns a nested object into a one-dimensional object and joins names with dots
+ * @param obj 
+ * @param prefix 
+ * @returns flattend objects
+ */
 export const flattenObject = (obj: any, prefix: string = "") =>
-  // @ts-ignore
   Object.keys(obj).reduce((acc, k) => {
     const pre = prefix.length ? prefix + '.' : '';
-    if (typeof obj[k] === 'object') Object.assign(acc, flattenObject(obj[k], pre + k));
-    // @ts-ignore
-    else acc[pre + k] = obj[k];
+    if (typeof obj[k] === 'object') {
+      // purposly mutating acc
+      Object.assign(acc, flattenObject(obj[k], pre + k));
+    }
+    else {
+      // @ts-ignore: implicit any
+      acc[pre + k] = obj[k];
+    }
     return acc;
   }, {})
