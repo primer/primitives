c882f41a9ccf8119cf7918ba5fac8a76ce29d66d
Lukas Oppermann <lukasoppermann@github.com>
2022-10-27 16:32:13 +0200

    Update @types/TokenBuildInputArray.d.ts
    Co-authored-by: Rez <rezrah@github.com>

--- @types/TokenBuildInputArray.d.ts
+++ @types/TokenBuildInputArray.d.ts
@@ -1,7 +1,7 @@
 type TokenBuildInputItem = {
   // Used as the filename for the generated file (e.g. `dark`.css), extension will be appended
   outputName: string
-  // Array of `filepaths` to token files that should be converted and included in the output
+  // Array of `filepaths` to token files that should be converted and included in the output. Accepts relative or glob paths.
   source: string[]
   // Array of `filepaths` to token fils that should NOT be included in the output, but should be available to reference during compilation e.g. base color scales
   include: string[]
