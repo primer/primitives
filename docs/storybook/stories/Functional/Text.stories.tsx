36b1aff0efb126c3e329ee0c2221f45f1ede78ed
langermank <langermank@github.com>
2022-11-06 09:40:04 -0800

    fix temp themess

--- docs/storybook/stories/Functional/Text.stories.tsx
+++ docs/storybook/stories/Functional/Text.stories.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-// import type {Meta} from '@storybook/react'
+import {ComponentMeta, ComponentStory} from '@storybook/react'
 import {ColorPreview} from '../ColorPreview'
 
 export default {
   title: 'Functional/Text',
   component: ColorPreview
-}
+} as ComponentMeta<typeof ColorPreview>
 
-export const TextColors = <ColorPreview color="text-primary" />
+// export const TextColors = <ColorPreview color="--product-color-fg-default" textColor />
+
+export const TextColors: ComponentStory<typeof ColorPreview> = () => (
+  <ColorPreview color="product-color-fg-default" textColor />
+)
