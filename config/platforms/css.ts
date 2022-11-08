13e5b0b60e60e5d581966555d9e7c8b272fbd5ec
Lukas Oppermann <lukasoppermann@github.com>
2022-11-08 13:31:55 +0100

    chaning selector to be for dark or light mode

--- config/platforms/css.ts
+++ config/platforms/css.ts
@@ -2,21 +2,41 @@ import StyleDictionary from 'style-dictionary'
 import {PlatformInitializer} from '~/types/PlatformInitializer'
 import {isSource} from '~/config/filters'
 
-export const css: PlatformInitializer = (outputFile, prefix, buildPath, _options): StyleDictionary.Platform => ({
-  prefix,
-  buildPath,
-  transforms: ['name/cti/kebab', 'color/hex', 'color/hexAlpha', 'shadow/css'],
-  options: {
-    basePxFontSize: 16
-  },
-  files: [
-    {
-      destination: `${outputFile}`,
-      format: `css/variables`,
-      filter: isSource,
-      options: {
-        outputReferences: false
+const getCssSelectors = (outputFile: string): {selector: string; selectorLight: string; selectorDark: string} => {
+  // check for dark in the beginning of the output filename
+  const lastSlash = outputFile.lastIndexOf('/')
+  const outputBasename = outputFile.substring(lastSlash + 1, outputFile.indexOf('.'))
+  const themeName = outputBasename.replace('-', '_')
+  const mode = outputBasename.substring(0, 4) === 'dark' ? 'dark' : 'light'
+
+  return {
+    selector: `[data-color-mode="${mode}"][data-${mode}-theme="${themeName}"]`,
+    selectorLight: `[data-color-mode="auto"][data-light-theme="${themeName}"]`,
+    selectorDark: `[data-color-mode="auto"][data-dark-theme="${themeName}"]`
+  }
+}
+
+export const css: PlatformInitializer = (outputFile, prefix, buildPath, _options): StyleDictionary.Platform => {
+  const {selector, selectorLight, selectorDark} = getCssSelectors(outputFile)
+  return {
+    prefix,
+    buildPath,
+    transforms: ['name/cti/kebab', 'color/hex', 'color/hexAlpha', 'shadow/css'],
+    options: {
+      basePxFontSize: 16
+    },
+    files: [
+      {
+        destination: `${outputFile}`,
+        format: `css/themed`,
+        filter: isSource,
+        options: {
+          outputReferences: false,
+          selector,
+          selectorLight,
+          selectorDark
+        }
       }
-    }
-  ]
-})
+    ]
+  }
+}
