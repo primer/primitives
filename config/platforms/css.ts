4221d71fed1cc4985e44fb145df9d34d69821323
Lukas Oppermann <lukasoppermann@github.com>
2022-11-08 13:31:55 +0100

    chaning selector to be for dark or light mode

--- config/platforms/css.ts
+++ config/platforms/css.ts
@@ -2,6 +2,19 @@ import StyleDictionary from 'style-dictionary'
 import {PlatformInitializer} from '~/types/PlatformInitializer'
 import {isSource} from '~/config/filters'
 
+const getCssSelector = (outputFile: string): string => {
+  let mode = 'light'
+  let modeInverse = 'dark'
+  // check for dark in the beginning of the output filename
+  const lastSlash = outputFile.lastIndexOf('/')
+  if (outputFile.substring(lastSlash + 1, lastSlash + 5) === 'dark') {
+    mode = 'dark'
+    modeInverse = 'light'
+  }
+
+  return `:root, [data-color-mode="${mode}"][data-light-theme*="${mode}"], [data-color-mode="${modeInverse}"][data-dark-theme*="${mode}"]`
+}
+
 export const css: PlatformInitializer = (outputFile, prefix, buildPath, _options): StyleDictionary.Platform => ({
   prefix,
   buildPath,
@@ -15,7 +28,8 @@ export const css: PlatformInitializer = (outputFile, prefix, buildPath, _options
       format: `css/variables`,
       filter: isSource,
       options: {
-        outputReferences: false
+        outputReferences: false,
+        selector: getCssSelector(outputFile)
       }
     }
   ]
