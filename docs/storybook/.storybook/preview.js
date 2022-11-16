ded94ecddb2f281d8227009c8b36a3e9c128d583
langermank <langermank@github.com>
2022-11-16 13:29:49 -0800

    init

---
+++ docs/storybook/.storybook/preview.js
@@ -0,0 +1,58 @@
+// import '../temp-themes/color-modes/index.scss'
+
+export const parameters = {
+  actions: {argTypesRegex: '^on[A-Z].*'},
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  }
+}
+
+const themes = [
+  'light',
+  'light_colorblind',
+  'light_high_contrast',
+  'dark',
+  'dark_dimmed',
+  'dark_high_contrast',
+  'dark_colorblind'
+]
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Switch themes',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: [...themes, 'all'],
+      showName: true
+    }
+  }
+}
+
+export const decorators = [
+  (Story, context) => {
+    return (
+      <div class="theme-wrap">
+        {themes.map(theme => {
+          if (context.globals.theme === theme || context.globals.theme === 'all') {
+            return (
+              <div
+                id="story"
+                className="story-wrap"
+                data-color-mode={theme.startsWith('dark') ? 'dark' : 'light'}
+                data-light-theme={theme.startsWith('light') ? theme : undefined}
+                data-dark-theme={theme.startsWith('dark') ? theme : undefined}
+              >
+                <Story {...context} />
+              </div>
+            )
+          }
+        })}
+      </div>
+    )
+  }
+]
---
+++ docs/storybook/stories/Functional/Text.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {ComponentMeta, ComponentStory} from '@storybook/react'
+import {ColorPreview} from '../ColorPreview'
+
+export default {
+  title: 'Functional/Text',
+  component: ColorPreview
+} as ComponentMeta<typeof ColorPreview>
+
+const fgColors = [
+  'primer-color-text-default',
+  'primer-color-text-subtle',
+  'primer-color-text-onDefault',
+  'primer-color-text-disabled ',
+  'primer-color-text-accent',
+  'primer-color-text-success',
+  'primer-color-text-attention',
+  'primer-color-text-severe',
+  'primer-color-text-danger'
+]
+
+export const TextColors: ComponentStory<typeof ColorPreview> = () => {
+  return (
+    <>
+      {fgColors.map(color => (
+        <ColorPreview color={color} textColor key={color} />
+      ))}
+    </>
+  )
+}
