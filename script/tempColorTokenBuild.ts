e7f8898ada73eea1c5f2b5d6d282fc7e1a93b708
Lukas Oppermann <lukasoppermann@github.com>
2022-12-02 11:21:11 +0100

    move v3 tokens to src/tokens

--- script/tempColorTokenBuild.ts
+++ script/tempColorTokenBuild.ts
@@ -10,90 +10,96 @@ export const buildDesignTokens = (buildOptions: ConfigGeneratorOptions): void =>
     {
       filename: 'light',
       source: [
-        `tokens/functional/color/light/*.json5`,
-        `tokens/functional/shadow/light.json5`,
-        `tokens/functional/border/light.json5`,
+        `src/tokens/functional/color/light/*.json5`,
+        `src/tokens/functional/shadow/light.json5`,
+        `src/tokens/functional/border/light.json5`,
       ],
-      include: [`tokens/base/color/light/light.json5`],
+      include: [`src/tokens/base/color/light/light.json5`],
     },
     {
       filename: 'light-tritanopia',
       source: [
-        `tokens/functional/color/light/*.json5`,
-        `tokens/functional/color/light/overrides/light.tritanopia.json5`,
-        `tokens/functional/shadow/light.json5`,
-        `tokens/functional/border/light.json5`,
+        `src/tokens/functional/color/light/*.json5`,
+        `src/tokens/functional/color/light/overrides/light.tritanopia.json5`,
+        `src/tokens/functional/shadow/light.json5`,
+        `src/tokens/functional/border/light.json5`,
       ],
-      include: [`tokens/base/color/light/light.json5`, `tokens/base/color/light/light.tritanopia.json5`],
+      include: [`src/tokens/base/color/light/light.json5`, `src/tokens/base/color/light/light.tritanopia.json5`],
     },
     {
       filename: 'light-colorblind',
       source: [
-        `tokens/functional/color/light/*.json5`,
-        `tokens/functional/color/light/overrides/light.protanopia-deuteranopia.json5`,
-        `tokens/functional/shadow/light.json5`,
-        `tokens/functional/border/light.json5`,
+        `src/tokens/functional/color/light/*.json5`,
+        `src/tokens/functional/color/light/overrides/light.protanopia-deuteranopia.json5`,
+        `src/tokens/functional/shadow/light.json5`,
+        `src/tokens/functional/border/light.json5`,
       ],
-      include: [`tokens/base/color/light/light.json5`, `tokens/base/color/light/light.protanopia-deuteranopia.json5`],
+      include: [
+        `src/tokens/base/color/light/light.json5`,
+        `src/tokens/base/color/light/light.protanopia-deuteranopia.json5`,
+      ],
     },
     {
       filename: 'light-high-contrast',
       source: [
-        `tokens/functional/color/light/*.json5`,
-        `tokens/functional/color/light/overrides/light.high-contrast.json5`,
-        `tokens/functional/shadow/light.json5`,
-        `tokens/functional/border/light.json5`,
+        `src/tokens/functional/color/light/*.json5`,
+        `src/tokens/functional/color/light/overrides/light.high-contrast.json5`,
+        `src/tokens/functional/shadow/light.json5`,
+        `src/tokens/functional/border/light.json5`,
       ],
-      include: [`tokens/base/color/light/light.json5`, `tokens/base/color/light/light.high-contrast.json5`],
+      include: [`src/tokens/base/color/light/light.json5`, `src/tokens/base/color/light/light.high-contrast.json5`],
     },
     {
       filename: 'dark',
       source: [
-        `tokens/functional/color/dark/*.json5`,
-        `tokens/functional/shadow/dark.json5`,
-        `tokens/functional/border/dark.json5`,
+        `src/tokens/functional/color/dark/*.json5`,
+        `src/tokens/functional/shadow/dark.json5`,
+        `src/tokens/functional/border/dark.json5`,
       ],
-      include: [`tokens/base/color/dark/dark.json5`],
+      include: [`src/tokens/base/color/dark/dark.json5`],
     },
     {
       filename: 'dark-dimmed',
       source: [
-        `tokens/functional/color/dark/*.json5`,
-        `tokens/functional/color/dark/overrides/dark.dimmed.json5`,
-        `tokens/functional/shadow/dark.json5`,
-        `tokens/functional/border/dark.json5`,
+        `src/tokens/functional/color/dark/*.json5`,
+        `src/tokens/functional/color/dark/overrides/dark.dimmed.json5`,
+        `src/tokens/functional/shadow/dark.json5`,
+        `src/tokens/functional/border/dark.json5`,
       ],
-      include: [`tokens/base/color/dark/dark.json5`, `tokens/base/color/dark/dark.dimmed.json5`],
+      include: [`src/tokens/base/color/dark/dark.json5`, `src/tokens/base/color/dark/dark.dimmed.json5`],
     },
     {
       filename: 'dark-tritanopia',
       source: [
-        `tokens/functional/color/dark/*.json5`,
-        `tokens/functional/color/dark/overrides/dark.tritanopia.json5`,
-        `tokens/functional/shadow/dark.json5`,
-        `tokens/functional/border/dark.json5`,
+        `src/tokens/functional/color/dark/*.json5`,
+        `src/tokens/functional/color/dark/overrides/dark.tritanopia.json5`,
+        `src/tokens/functional/shadow/dark.json5`,
+        `src/tokens/functional/border/dark.json5`,
       ],
-      include: [`tokens/base/color/dark/dark.json5`, `tokens/base/color/dark/dark.tritanopia.json5`],
+      include: [`src/tokens/base/color/dark/dark.json5`, `src/tokens/base/color/dark/dark.tritanopia.json5`],
     },
     {
       filename: 'dark-colorblind',
       source: [
-        `tokens/functional/color/dark/*.json5`,
-        `tokens/functional/color/dark/overrides/dark.protanopia-deuteranopia.json5`,
-        `tokens/functional/shadow/dark.json5`,
-        `tokens/functional/border/dark.json5`,
+        `src/tokens/functional/color/dark/*.json5`,
+        `src/tokens/functional/color/dark/overrides/dark.protanopia-deuteranopia.json5`,
+        `src/tokens/functional/shadow/dark.json5`,
+        `src/tokens/functional/border/dark.json5`,
       ],
-      include: [`tokens/base/color/dark/dark.json5`, `tokens/base/color/dark/dark.protanopia-deuteranopia.json5`],
+      include: [
+        `src/tokens/base/color/dark/dark.json5`,
+        `src/tokens/base/color/dark/dark.protanopia-deuteranopia.json5`,
+      ],
     },
     {
       filename: 'dark-high-contrast',
       source: [
-        `tokens/functional/color/dark/*.json5`,
-        `tokens/functional/color/dark/overrides/dark.high-contrast.json5`,
-        `tokens/functional/shadow/dark.json5`,
-        `tokens/functional/border/dark.json5`,
+        `src/tokens/functional/color/dark/*.json5`,
+        `src/tokens/functional/color/dark/overrides/dark.high-contrast.json5`,
+        `src/tokens/functional/shadow/dark.json5`,
+        `src/tokens/functional/border/dark.json5`,
       ],
-      include: [`tokens/base/color/dark/dark.json5`, `tokens/base/color/dark/dark.high-contrast.json5`],
+      include: [`src/tokens/base/color/dark/dark.json5`, `src/tokens/base/color/dark/dark.high-contrast.json5`],
     },
   ]
 
@@ -118,7 +124,7 @@ export const buildDesignTokens = (buildOptions: ConfigGeneratorOptions): void =>
   /**
    * Copy `removed` files
    */
-  copyFromDir(`tokens/removed`, `${buildOptions.buildPath}removed`)
+  copyFromDir(`src/tokens/removed`, `${buildOptions.buildPath}removed`)
 
   /**
    * convert colors
@@ -131,7 +137,7 @@ export const buildDesignTokens = (buildOptions: ConfigGeneratorOptions): void =>
     // build base scales
     PrimerStyleDictionary.extend(
       // using includes as source
-      getStyleDictionaryConfig(`base/color/${filename}`, [`tokens/functional/color/scales.json5`], include, {
+      getStyleDictionaryConfig(`base/color/${filename}`, [`src/tokens/functional/color/scales.json5`], include, {
         buildPath: buildOptions.buildPath,
         prefix: 'base',
       }),
@@ -184,7 +190,7 @@ export const buildDesignTokens = (buildOptions: ConfigGeneratorOptions): void =>
 
   // build types for base scale
   PrimerStyleDictionary.extend({
-    source: [`tokens/functional/color/scales.json5`],
+    source: [`src/tokens/functional/color/scales.json5`],
     include: themes[0].include,
     platforms: {
       types: typeDefinitions(`baseColor`, undefined, `${buildOptions.buildPath}ts/types/`),
@@ -193,6 +199,6 @@ export const buildDesignTokens = (buildOptions: ConfigGeneratorOptions): void =>
 }
 
 buildDesignTokens({
-  buildPath: 'tokens-v2-private/',
+  buildPath: 'tokens-v3-private/',
   prefix: 'ui',
 })
