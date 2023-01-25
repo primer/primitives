8aef247a04bb5b63491b9dafa4fd609a9c65c80b
Lukas Oppermann <lukasoppermann@github.com>
2023-01-25 13:38:10 +0100

    add commments and better structure to build-v3

--- scripts/buildV3.ts
+++ scripts/buildV3.ts
@@ -5,8 +5,36 @@ import {typeDefinitions, deprecatedJson, css, docJson, scss, javascript, typescr
 import type {ConfigGeneratorOptions, StyleDictionaryConfigGenerator} from '~/src/types/StyleDictionaryConfigGenerator'
 import type {TokenBuildInput} from '~/src/types/TokenBuildInput'
 import glob from 'fast-glob'
+/**
+ * getStyleDictionaryConfig
+ * @param filename output file name without extension
+ * @param source array of source token json files
+ * @param include array of included token json files (will not be present in output btu values are used when referenced)
+ * @param options options object
+ * @returns style dictionary config object
+ */
+const getStyleDictionaryConfig: StyleDictionaryConfigGenerator = (
+  filename,
+  source,
+  include,
+  options,
+): StyleDictionary.Config => ({
+  source, // build the special formats
+  include,
+  platforms: {
+    css: css(`css/${filename}.css`, options.prefix, options.buildPath, {themed: options.themed}),
+    scss: scss(`scss/${filename}.scss`, options.prefix, options.buildPath),
+    js: javascript(`js/${filename}.js`, options.prefix, options.buildPath),
+    ts: typescript(`ts/${filename}.ts`, options.prefix, options.buildPath),
+    json: json(`json/${filename}.json`, options.prefix, options.buildPath),
+    docJson: docJson(`docs/${filename}.json`, options.prefix, options.buildPath),
+  },
+})
 
 export const buildDesignTokens = (buildOptions: ConfigGeneratorOptions): void => {
+  /** -----------------------------------
+   * Colors, shadows & borders
+   * ----------------------------------- */
   const themes: TokenBuildInput[] = [
     {
       filename: 'light',
@@ -104,32 +132,6 @@ export const buildDesignTokens = (buildOptions: ConfigGeneratorOptions): void =>
     },
   ]
 
-  const getStyleDictionaryConfig: StyleDictionaryConfigGenerator = (
-    filename,
-    source,
-    include,
-    options,
-  ): StyleDictionary.Config => ({
-    source, // build the special formats
-    include,
-    platforms: {
-      css: css(`css/${filename}.css`, options.prefix, options.buildPath, {themed: options.themed}),
-      scss: scss(`scss/${filename}.scss`, options.prefix, options.buildPath),
-      js: javascript(`js/${filename}.js`, options.prefix, options.buildPath),
-      ts: typescript(`ts/${filename}.ts`, options.prefix, options.buildPath),
-      json: json(`json/${filename}.json`, options.prefix, options.buildPath),
-      docJson: docJson(`docs/${filename}.json`, options.prefix, options.buildPath),
-    },
-  })
-
-  /**
-   * Copy `removed` files
-   */
-  copyFromDir(`src/tokens/removed`, `${buildOptions.buildPath}removed`)
-
-  /**
-   * convert colors
-   */
   for (const {filename, source, include} of themes) {
     // build functional scales
     PrimerStyleDictionary.extend(
@@ -144,9 +146,10 @@ export const buildDesignTokens = (buildOptions: ConfigGeneratorOptions): void =>
       }),
     ).buildAllPlatforms()
   }
-  /**
-   * build size tokens
-   */
+
+  /** -----------------------------------
+   * Size tokens
+   * ----------------------------------- */
   const sizeFiles = glob.sync('src/tokens/functional/size/*')
   //
   for (const file of sizeFiles) {
@@ -168,9 +171,9 @@ export const buildDesignTokens = (buildOptions: ConfigGeneratorOptions): void =>
     }),
   ).buildAllPlatforms()
 
-  /**
-   * build typography
-   */
+  /** -----------------------------------
+   * Typography tokens
+   * ----------------------------------- */
   PrimerStyleDictionary.extend(
     getStyleDictionaryConfig(
       `functional/typography/typography`,
@@ -184,20 +187,32 @@ export const buildDesignTokens = (buildOptions: ConfigGeneratorOptions): void =>
     getStyleDictionaryConfig(`base/typography/typography`, [`src/tokens/base/typography/*.json`], [], buildOptions),
   ).buildAllPlatforms()
 
-  /**
-   * build deprecated.json
-   */
+  /** -----------------------------------
+   * deprecated tokens
+   * ----------------------------------- */
   const deprecatedBuilds: TokenBuildInput[] = [
+    // color, shadow & borders
     {
-      // light mode
       filename: 'theme',
-      source: [...themes[0].source, ...themes[0].include],
-      include: themes[0].include,
+      source: [
+        `src/tokens/base/color/light/light.json5`,
+        `src/tokens/functional/color/light/*.json5`,
+        `src/tokens/functional/shadow/light.json5`,
+        `src/tokens/functional/border/light.json5`,
+      ],
+      include: [`src/tokens/base/color/light/light.json5`],
     },
+    // typography
     {
       filename: 'typography',
-      source: [`src/tokens/functional/typography/*.json`],
+      source: [`src/tokens/base/typography/*.json`, `src/tokens/functional/typography/*.json`],
       include: [`src/tokens/base/typography/*.json`],
+    },
+    // size
+    {
+      filename: 'size',
+      source: [`src/tokens/base/size/*.json`, `src/tokens/functional/size/*.json`],
+      include: [`src/tokens/base/size/*.json`],
     },
   ]
   //
@@ -210,21 +225,37 @@ export const buildDesignTokens = (buildOptions: ConfigGeneratorOptions): void =>
       },
     }).buildAllPlatforms()
   }
-  /**
-   * build type definitions
-   */
-  // TODO: The tuple structure gets to complex this should be refactored to be easier to read
+
+  /** -----------------------------------
+   * Copy `removed` files
+   * ----------------------------------- */
+  copyFromDir(`src/tokens/removed`, `${buildOptions.buildPath}removed`)
+
+  /** -----------------------------------
+   * Type definitions
+   * ----------------------------------- */
   const typeBuilds: TokenBuildInput[] = [
+    // color, shadow & border
     {
-      // light mode
       filename: 'theme',
-      source: themes[0].source,
-      include: themes[0].include,
+      source: [
+        `src/tokens/functional/color/light/*.json5`,
+        `src/tokens/functional/shadow/light.json5`,
+        `src/tokens/functional/border/light.json5`,
+      ],
+      include: [`src/tokens/base/color/light/light.json5`],
     },
+    // typography
     {
       filename: 'typography',
       source: [`src/tokens/functional/typography/*.json`],
       include: [`src/tokens/base/typography/*.json`],
+    },
+    // size
+    {
+      filename: 'size',
+      source: [`src/tokens/functional/size/*.json`],
+      include: [`src/tokens/base/size/*.json`],
     },
   ]
   //
@@ -241,13 +272,16 @@ export const buildDesignTokens = (buildOptions: ConfigGeneratorOptions): void =>
   // build types for base scale
   PrimerStyleDictionary.extend({
     source: [`src/tokens/functional/color/scales.json5`],
-    include: themes[0].include,
+    include: [`src/tokens/base/color/light/light.json5`],
     platforms: {
       types: typeDefinitions(`baseColor`, undefined, `${buildOptions.buildPath}ts/types/`),
     },
   }).buildAllPlatforms()
 }
 
+/** -----------------------------------
+ * Run build script
+ * ----------------------------------- */
 buildDesignTokens({
   buildPath: 'tokens-v3-private/',
   prefix: 'ui',
