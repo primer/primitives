daee5fa21d7af1675b87590e0da40835a9ad4a52
Lukas Oppermann <lukasoppermann@github.com>
2023-02-02 12:31:33 +0100

    improve token error (#479)

--- src/transformers/utilities/getTokenValue.ts
+++ src/transformers/utilities/getTokenValue.ts
@@ -1,13 +1,9 @@
 import type StyleDictionary from 'style-dictionary'
-import {namePathToDotNotation} from '../namePathToDotNotation'
+import {invalidTokenError} from './invalidTokenError'
 
 export const getTokenValue = (token: StyleDictionary.TransformedToken) => {
   if (token.value === undefined) {
-    throw new Error(
-      `Undefined value for token: "${namePathToDotNotation.transformer(token)}" in file "${token.filePath}". ${
-        token.original.value ? `The original token value is: "${token.original.value}" ` : ''
-      }This may be due to referencing a token that does not exists.`,
-    )
+    throw new invalidTokenError(token)
   }
 
   return token.value
