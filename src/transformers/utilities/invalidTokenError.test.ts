81cb9dd0cbe359973ef4a046e0ef5607456516fa
Lukas Oppermann <lukasoppermann@github.com>
2023-02-02 14:16:33 +0100

    add error handling for typography (#481)

--- src/transformers/utilities/invalidTokenError.test.ts
+++ src/transformers/utilities/invalidTokenError.test.ts
@@ -1,21 +1,21 @@
 import {getMockToken} from '~/src/test-utilities'
-import {invalidTokenError} from './invalidTokenError'
+import {invalidTokenValueError} from './invalidTokenError'
 
-describe('Utilities: invalidTokenError', () => {
+describe('Utilities: invalidTokenValueError', () => {
   it('it throws with token', () => {
     expect(() => {
-      throw new invalidTokenError(
+      throw new invalidTokenValueError(
         getMockToken({
           value: '42px',
         }),
       )
-    }).toThrow(invalidTokenError)
+    }).toThrow(invalidTokenValueError)
   })
 
   it('it throws TypeError if no token is provided', () => {
     expect(() => {
       // @ts-expect-error due to testing wrong input
-      throw new invalidTokenError('42px')
+      throw new invalidTokenValueError('42px')
     }).toThrow(TypeError)
   })
 })
--- src/transformers/utilities/invalidTokenError.ts
+++ src/transformers/utilities/invalidTokenError.ts
@@ -1,18 +1,34 @@
 import type {TransformedToken} from 'style-dictionary'
 import {namePathToDotNotation} from '../namePathToDotNotation'
 
-const composeErrorMessage = (token: TransformedToken) => {
+const composeValueErrorMessage = (token: TransformedToken) => {
   // eslint-disable-next-line i18n-text/no-en
   return `Invalid token "${namePathToDotNotation.transformer(token)}" in file "${
     token.filePath
-  }". Transformed value: "${token.value}". ${
-    token.original.value ? `Original value: "${token.original.value}" ` : ''
+  }". Transformed value: "${JSON.stringify(token.value)}". ${
+    token.original.value ? `Original value: "${JSON.stringify(token.original.value)}" ` : ''
   }This may be due to referencing a token that does not exists.`
 }
 
-export class invalidTokenError extends Error {
+const composeValuePropertyErrorMessage = (token: TransformedToken, property: string) => {
+  // eslint-disable-next-line i18n-text/no-en
+  return `Invalid property "${property}" of token "${namePathToDotNotation.transformer(token)}" in file "${
+    token.filePath
+  }". Transformed property value: "${token.value[property]}". ${
+    token.original.value ? `Original value: "${token.original.value[property]}" ` : ''
+  }This may be due to referencing a token that does not exists.`
+}
+
+export class invalidTokenValueError extends Error {
   constructor(token: TransformedToken) {
-    super(composeErrorMessage(token))
-    Error.captureStackTrace(this, invalidTokenError)
+    super(composeValueErrorMessage(token))
+    Error.captureStackTrace(this, invalidTokenValueError)
+  }
+}
+
+export class invalidTokenValuePropertyError extends Error {
+  constructor(token: TransformedToken, property: string) {
+    super(composeValuePropertyErrorMessage(token, property))
+    Error.captureStackTrace(this, invalidTokenValuePropertyError)
   }
 }
