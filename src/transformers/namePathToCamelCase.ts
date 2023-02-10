ae61c39a8e8489ab6a5bb9ec6e2d0caadd3e0aa7
Lukas Oppermann <lukasoppermann@github.com>
2023-02-10 08:51:03 +0100

    replacing PascalCase with camelCase as swift does need camelCase for most things

--- src/transformers/namePathToCamelCase.ts
+++ src/transformers/namePathToCamelCase.ts
@@ -1,5 +1,5 @@
 import type StyleDictionary from 'style-dictionary'
-import {upperCaseFirstCharacter} from '~/src/utilities'
+import {toCamelCase} from '~/src/utilities'
 
 /**
  * @description converts the [TransformedToken's](https://github.com/amzn/style-dictionary/blob/main/types/TransformedToken.d.ts) `.path` array to a camelCase string, preserves casing of parts
@@ -9,23 +9,6 @@ import {upperCaseFirstCharacter} from '~/src/utilities'
  */
 export const namePathToCamelCase: StyleDictionary.Transform = {
   type: `name`,
-  transformer: (token: StyleDictionary.TransformedToken, options?: StyleDictionary.Platform): string => {
-    // match unsupported characters
-    const regex = /[^a-zA-Z0-9]+/g
-    // replace any non-letter and non-number character and split into word array
-    const tokenPathArray = token.path.join(' ').replace(regex, ' ').split(' ')
-    return (
-      [options?.prefix, ...tokenPathArray]
-        // remove undefined if exists
-        .filter((part: unknown): part is string => typeof part === 'string')
-        // ucFirst all but first part
-        .map((part: string, index: number) => {
-          if (index > 0) {
-            return upperCaseFirstCharacter(part)
-          }
-          return part
-        })
-        .join('')
-    )
-  },
+  transformer: (token: StyleDictionary.TransformedToken, options?: StyleDictionary.Platform): string =>
+    toCamelCase([options?.prefix || '', ...token.path]),
 }
