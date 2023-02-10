1116021641043eb667ad75146160a603ea945f81
Lukas Oppermann <lukasoppermann@github.com>
2023-02-10 08:31:36 +0100

    add utility to transform string or array of strings to PascalCase (#488)

---
+++ src/utilities/toPascalCase.ts
@@ -0,0 +1,19 @@
+import {upperCaseFirstCharacter} from './upperCaseFirstCharacter'
+
+export const toPascalCase = (string: string | string[]) => {
+  if (!Array.isArray(string)) {
+    string = [string]
+  }
+  // match unsupported characters
+  const regex = /[^a-zA-Z0-9]+/g
+  // replace any non-letter and non-number character and split into word array
+  const stringArray = string.join(' ').replace(regex, ' ').split(' ')
+  return (
+    stringArray
+      // remove undefined if exists
+      .filter((part: unknown): part is string => typeof part === 'string')
+      // ucFirst all but first part
+      .map(upperCaseFirstCharacter)
+      .join('')
+  )
+}
