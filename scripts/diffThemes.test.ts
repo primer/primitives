8bcd0a3e005b9be5391658cadd8181640818b2ed
Lukas Oppermann <lukasoppermann@github.com>
2023-03-09 14:13:04 +0100

    add test for diffThemes

---
+++ scripts/diffThemes.test.ts
@@ -0,0 +1,34 @@
+import {diffThemes} from './diffThemes'
+
+describe('diffThemes', () => {
+  const themesArray: [string, string[]][] = [
+    ['light', ['color', 'color.text']],
+    ['dark', ['color', 'color.text']],
+  ]
+
+  it('Throws error if mainTheme name does not exist', () => {
+    expect(() => diffThemes('invalid', themesArray)).toThrow()
+  })
+
+  it('Outputs message for missing tokens', () => {
+    const withMissing = JSON.parse(JSON.stringify(themesArray))
+    withMissing[0][1].push('color.text.success')
+    const returnString =
+      '\n:::: dark vs light ::::\n\n' +
+      '1 tokens missing in dark:\n' +
+      '  - color.text.success\n\n' +
+      '0 tokens added in dark'
+    expect(diffThemes('light', withMissing)).toEqual(returnString)
+  })
+
+  it('Outputs message for additional tokens', () => {
+    const withAdditional = JSON.parse(JSON.stringify(themesArray))
+    withAdditional[1][1].push('color.text.success')
+    const returnString =
+      '\n:::: dark vs light ::::\n\n' +
+      '0 tokens missing in dark:\n\n' +
+      '1 tokens added in dark\n' +
+      '  - color.text.success'
+    expect(diffThemes('light', withAdditional)).toEqual(returnString)
+  })
+})
