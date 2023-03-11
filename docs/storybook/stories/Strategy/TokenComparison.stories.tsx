52b166822400c56a3a0f12bb79b4f66ee063a37b
langermank <18661030+langermank@users.noreply.github.com>
2023-03-10 16:27:22 -0800

    lint

--- docs/storybook/stories/Strategy/TokenComparison.stories.tsx
+++ docs/storybook/stories/Strategy/TokenComparison.stories.tsx
@@ -1,57 +1,13 @@
 import React from 'react'
 import {CSSTokenSwatch} from '../Components/CSSTokenSwatch'
+// eslint-disable-next-line import/extensions
 import cssVars from './DeprecatedTokensMap.json'
 import {DataTable, Table} from '@primer/react/drafts'
 
 export default {
   title: 'Deprecation/ComparisonTable',
-  // parameters: {
-  //   storyType: 'swatch',
-  // },
 }
-
-// const cssVariableNames = Object.keys(cssVariables)
-
-// return (
-//   <div>
-//     {cssVariableNames.map(variableName => (
-//       <div key={variableName}>
-//         <span>{variableName}: </span>
-//         <span>{JSON.stringify(cssVariables[variableName])}</span>
-//       </div>
-//     ))}
-//   </div>
-// )
-
 export const ComparisionTable = () => {
-  const cssVariableNames = Object.keys(cssVars)
-  console.log(cssVariableNames)
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Old</th>
-          <th>New</th>
-        </tr>
-      </thead>
-      <tbody>
-        {cssVariableNames.map(variableName => (
-          <tr key={variableName}>
-            <td>
-              <CSSTokenSwatch color={variableName} />
-            </td>
-          </tr>
-        ))}
-        {/* <td>
-          <CSSTokenSwatch color="base-color-pink-5" />
-        </td> */}
-      </tbody>
-    </table>
-  )
-}
-
-export const NewTable = () => {
-  const cssVariableNames = Object.keys(cssVars)
   const data = Object.entries(cssVars).map(([key, value]) => {
     return {
       id: key,
@@ -76,18 +32,11 @@ export const NewTable = () => {
               return <CSSTokenSwatch color={row.id} />
             },
           },
-          {
-            header: 'New',
-            field: 'color',
-            renderCell: row => {
-              return <CSSTokenSwatch color={row.color} />
-            },
-          },
           // {
-          //   header: 'Type',
-          //   field: 'type',
+          //   header: 'New',
+          //   field: 'color',
           //   renderCell: row => {
-          //     return <Label>{uppercase(row.type)}</Label>
+          //     return <CSSTokenSwatch color={row.color} />
           //   },
           // },
         ]}
