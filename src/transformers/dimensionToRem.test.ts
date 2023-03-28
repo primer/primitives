e2305333d21977d3d6fd7432654921f90967e03f
Lukas Oppermann <lukasoppermann@github.com>
2023-03-28 12:19:36 +0200

    added tests to dimToRem (#533)

---
+++ src/transformers/dimensionToRem.test.ts
@@ -0,0 +1,71 @@
+import {getMockToken} from '~/src/test-utilities'
+import {dimensionToRem} from './dimensionToRem'
+
+describe('Transformer: dimensionToRem', () => {
+  it('transforms pixel string tokens to rem', () => {
+    const input = [
+      getMockToken({
+        value: '16px',
+      }),
+    ]
+    const expectedOutput = ['1rem']
+    expect(input.map(item => dimensionToRem.transformer(item))).toStrictEqual(expectedOutput)
+  })
+
+  it('transforms number to rem', () => {
+    const input = [
+      getMockToken({
+        value: '16',
+      }),
+      getMockToken({
+        value: 16,
+      }),
+    ]
+    const expectedOutput = ['1rem', '1rem']
+    expect(input.map(item => dimensionToRem.transformer(item))).toStrictEqual(expectedOutput)
+  })
+
+  it('transforms rem to rem', () => {
+    const input = [
+      getMockToken({
+        value: '1rem',
+      }),
+    ]
+    const expectedOutput = ['1rem']
+    expect(input.map(item => dimensionToRem.transformer(item))).toStrictEqual(expectedOutput)
+  })
+
+  it('transforms 0 to 0', () => {
+    const input = [
+      getMockToken({
+        value: '0rem',
+      }),
+      getMockToken({
+        value: '0px',
+      }),
+      getMockToken({
+        value: '0',
+      }),
+    ]
+    const expectedOutput = ['0', '0', '0']
+    expect(input.map(item => dimensionToRem.transformer(item))).toStrictEqual(expectedOutput)
+  })
+
+  it('throws on invalid tokens', () => {
+    const input = [
+      getMockToken({
+        value: 'rem',
+      }),
+      getMockToken({
+        value: '',
+      }),
+      getMockToken({
+        value: undefined,
+      }),
+      getMockToken({
+        value: null,
+      }),
+    ]
+    expect(() => input.map(item => dimensionToRem.transformer(item))).toThrow()
+  })
+})
