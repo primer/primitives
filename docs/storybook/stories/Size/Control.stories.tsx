faabecf67d59f092ae69fea05a14d78fa06fca62
Lukas Oppermann <lukasoppermann@github.com>
2023-04-17 13:09:28 +0200

    update for new token structure

--- docs/storybook/stories/Size/Control.stories.tsx
+++ docs/storybook/stories/Size/Control.stories.tsx
@@ -4,6 +4,7 @@ import sizeTokens from '../../../../tokens-next-private/docs/functional/size/siz
 import {ControlSizeDemo} from '../StorybookComponents/ControlSizeDemo/ControlSizeDemo'
 import {DataTable, Table} from '@primer/react/drafts'
 import {InlineCode} from '../StorybookComponents/InlineCode/InlineCode'
+import {getTokensByName} from '../utilities/getTokensByName'
 
 export default {
   title: 'Size/Control',
@@ -13,42 +14,43 @@ export default {
 }
 
 export const ControlXsmall = () => {
-  const sizeArray = Object.entries(sizeTokens.control.xsmall)
-  const data = sizeArray.map(([key, value]) => {
+  // const sizeArray = Object.entries(sizeTokens.control.xsmall)
+  const data = getTokensByName(sizeTokens, 'control-xsmall').map(token => {
     return {
-      id: key,
-      ...value,
+      id: token.name,
+      ...token,
     }
   })
+  console.log(data)
 
-  // get values for each item
-  const getValueString = (sizeTokens, searchString) => {
-    const sizeArray = Object.entries(sizeTokens.control.xsmall)
-    const item = sizeArray.find(([key, value]) => key.includes(searchString))
+  // // get values for each item
+  // const getValueString = (sizeTokens, searchString) => {
+  //   const sizeArray = Object.entries(sizeTokens.control.xsmall)
+  //   const item = sizeArray.find(([key, value]) => key.includes(searchString))
 
-    if (item) {
-      const [, itemValue] = item
-      return itemValue.name
-    }
+  //   if (item) {
+  //     const [, itemValue] = item
+  //     return itemValue.name
+  //   }
 
-    return null
-  }
+  //   return null
+  // }
 
   // get string for each item
-  const gapValueString = getValueString(sizeTokens, 'gap')
-  const paddingLeftValueString = getValueString(sizeTokens, 'paddingInline')
-  const paddingRightValueString = getValueString(sizeTokens, 'paddingInline')
-  const paddingTopValueString = getValueString(sizeTokens, 'paddingBlock')
-  const paddingBottomValueString = getValueString(sizeTokens, 'paddingBlock')
-  const lineBlockHeightPropValueString = getValueString(sizeTokens, 'lineBoxHeight')
-  const blockSizeValueString = getValueString(sizeTokens, 'size')
-  const condensedValueString = getValueString(sizeTokens, 'condensed')
-  const normalValueString = getValueString(sizeTokens, 'normal')
-  const spaciousValueString = getValueString(sizeTokens, 'spacious')
+  const gapValueString = getTokensByName(sizeTokens, 'control-xsmall-gap')[0].value
+  const paddingLeftValueString = getTokensByName(sizeTokens, 'control-xsmall-paddingInline')[0].value
+  const paddingRightValueString = getTokensByName(sizeTokens, 'control-xsmall-paddingInline')[0].value
+  const paddingTopValueString = getTokensByName(sizeTokens, 'control-xsmall-paddingBlock')[0].value
+  const paddingBottomValueString = getTokensByName(sizeTokens, 'control-xsmall-paddingBlock')[0].value
+  const lineBlockHeightPropValueString = getTokensByName(sizeTokens, 'control-xsmall-lineBoxHeight')[0].value
+  const blockSizeValueString = getTokensByName(sizeTokens, 'control-xsmall-size')[0].value
+  // const condensedValueString = getTokensByName(sizeTokens, 'control-xsmall-condensed')[0].value
+  // const normalValueString = getTokensByName(sizeTokens, 'control-xsmall-normal')[0].value
+  // const spaciousValueString = getTokensByName(sizeTokens, 'control-xsmall-spacious')[0].value
 
   // get boolean if item exists
-  const gap = getValueString(sizeTokens, 'gap') ? true : false
-  console.log(data)
+  // const gap = getValueString(sizeTokens, 'gap') ? true : false
+  // console.log(data)
 
   return (
     <Table.Container>
@@ -74,15 +76,15 @@ export const ControlXsmall = () => {
                   paddingBottom={paddingBottomValueString}
                   blockSize={blockSizeValueString}
                   lineBox={lineBlockHeightPropValueString}
-                  modifier={
-                    condensedValueString
-                      ? '-condensed'
-                      : normalValueString
-                      ? '-normal'
-                      : spaciousValueString
-                      ? '-spacious'
-                      : '-normal'
-                  }
+                  // modifier={
+                  //   condensedValueString
+                  //     ? '-condensed'
+                  //     : normalValueString
+                  //     ? '-normal'
+                  //     : spaciousValueString
+                  //     ? '-spacious'
+                  //     : '-normal'
+                  // }
                   highlightPaddingBottom={paddingBottomValueString}
                   highlightPaddingTop={paddingTopValueString}
                   highlightPaddingRight={paddingRightValueString}
