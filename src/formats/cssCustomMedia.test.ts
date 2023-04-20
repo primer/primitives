7ecb3134dcb3d417a2369293cac1d25f73ee3d87
Lukas Oppermann <lukasoppermann@github.com>
2023-04-20 11:48:52 +0200

    add test for cssCustoMedia

---
+++ src/formats/cssCustomMedia.test.ts
@@ -0,0 +1,11 @@
+import {cssCustomMedia} from './cssCustomMedia'
+import {getMockFormatterArguments} from '~/src/test-utilities'
+import {format} from 'prettier'
+
+describe('Format: tokens to custom-media', () => {
+  it('Formats tokens with custom media', () => {
+    const input = getMockFormatterArguments()
+    const expectedOutput = format(`@custom-media --red transformedValue;`, {parser: 'css', printWidth: 500})
+    expect(cssCustomMedia(input)).toStrictEqual(expectedOutput)
+  })
+})
