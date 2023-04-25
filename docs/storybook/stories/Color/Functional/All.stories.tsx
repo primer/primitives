490e0cbc9ffeb1186c0524273e24e99f663eff7d
Katie Langerman <18661030+langermank@users.noreply.github.com>
2023-04-25 10:11:03 -0700

    Storybook additions (#602)
    * add "all" for functional colors
    
    * order nav and upgrade

---
+++ docs/storybook/stories/Color/Functional/All.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Background} from './Background.stories'
+import {Border} from './Border.stories'
+import {Foreground} from './Foreground.stories'
+import {Shadows} from './Shadows.stories'
+import {Neutral, Accent, Success, Attention, Severe, Danger, Open, Closed, Done, Sponsors} from './Roles.stories'
+
+export default {
+  title: 'Color/Functional/All',
+  parameters: {
+    controls: {hideNoControlsWarning: true},
+  },
+}
+
+export const All = () => {
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', gap: '3rem'}}>
+      {[
+        {title: 'Backgrounds', component: <Background />},
+        {title: 'Borders', component: <Border />},
+        {title: 'Foregrounds', component: <Foreground />},
+        {title: 'Shadows', component: <Shadows />},
+        {title: 'Neutral', component: <Neutral />},
+        {title: 'Accent', component: <Accent />},
+        {title: 'Success', component: <Success />},
+        {title: 'Attention', component: <Attention />},
+        {title: 'Severe', component: <Severe />},
+        {title: 'Danger', component: <Danger />},
+        {title: 'Open', component: <Open />},
+        {title: 'Closed', component: <Closed />},
+        {title: 'Done', component: <Done />},
+        {title: 'Sponsors', component: <Sponsors />},
+      ].map(({title, component}) => (
+        <div>
+          <h2 style={{marginBlockEnd: '0.5rem'}}>{title}</h2>
+          <div className="SwatchDecorator">{component}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
