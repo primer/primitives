689ba92c5ff852e70b3dc50429537a0667fdcee8
Lukas Oppermann <lukasoppermann@github.com>
2023-04-27 13:14:12 +0200

    add option to remove descriptions from css output

--- src/formats/cssVariables.ts
+++ src/formats/cssVariables.ts
@@ -1,6 +1,5 @@
 import StyleDictionary from 'style-dictionary'
 import type {FormatterArguments} from 'style-dictionary/types/Format'
-import {format} from 'prettier'
 import type {LineFormatting} from 'style-dictionary/types/FormatHelpers'
 const {fileHeader, formattedVariables} = StyleDictionary.formatHelpers
 
@@ -10,26 +9,18 @@ const {fileHeader, formattedVariables} = StyleDictionary.formatHelpers
  * @param arguments.options outputReferences `boolean`, selector `string`, selectorLight `string`, selectorDark `string`
  * @returns formatted `string`
  */
-export const cssThemed: StyleDictionary.Formatter = ({dictionary, options = {}, file}: FormatterArguments) => {
-  const {selector, selectorLight, selectorDark} = options
+export const cssVariables: StyleDictionary.Formatter = ({dictionary, options = {}, file}: FormatterArguments) => {
+  const selector = options.selector ? options.selector : `:root`
   const {outputReferences, descriptions} = options
   const formatting: LineFormatting = {
     commentStyle: descriptions ? 'long' : 'none',
   }
   // add file header
-  const output = [fileHeader({file})]
-  // add single theme css
-  output.push(`${selector || ':root'}${selectorLight ? `, ${selectorLight}` : ''}{
-    ${formattedVariables({format: 'css', dictionary, outputReferences, formatting})}
-  }`)
-  // add auto dark theme css
-  if (selectorDark) {
-    output.push(`@media (prefers-color-scheme: dark) {
-      ${selectorDark} {
-        ${formattedVariables({format: 'css', dictionary, outputReferences, formatting})}
-      }
-    }`)
-  }
-  // return prettified
-  return format(output.join('\n'), {parser: 'css', printWidth: 500})
+
+  return `${fileHeader({file})}${selector} {\n${formattedVariables({
+    format: 'css',
+    dictionary,
+    outputReferences,
+    formatting,
+  })}\n}\n`
 }
--- src/formats/cssWrapMediaQuery.ts
+++ src/formats/cssWrapMediaQuery.ts
@@ -1,6 +1,7 @@
 import StyleDictionary from 'style-dictionary'
 import type {FormatterArguments} from 'style-dictionary/types/Format'
 import {format} from 'prettier'
+import type {LineFormatting} from 'style-dictionary/types/FormatHelpers'
 const {fileHeader, formattedVariables} = StyleDictionary.formatHelpers
 
 /**
@@ -9,7 +10,10 @@ const {fileHeader, formattedVariables} = StyleDictionary.formatHelpers
  * @returns formatted `string`
  */
 export const cssWrapMediaQuery: StyleDictionary.Formatter = ({dictionary, options, file}: FormatterArguments) => {
-  const {outputReferences} = options
+  const {outputReferences, descriptions} = options
+  const formatting: LineFormatting = {
+    commentStyle: descriptions ? 'long' : 'none',
+  }
   // get specific media query or use screen
   const mediaQuery = options.mediaQuery?.[file.destination] || 'screen'
   // add file header
@@ -17,7 +21,7 @@ export const cssWrapMediaQuery: StyleDictionary.Formatter = ({dictionary, option
   // add meadia query
   output.push(`@media ${mediaQuery} {\n:root {`)
   // add tokens
-  output.push(formattedVariables({format: 'css', dictionary, outputReferences}))
+  output.push(formattedVariables({format: 'css', dictionary, outputReferences, formatting}))
   // close media query
   output.push(`}\n}`)
   // return prettified
