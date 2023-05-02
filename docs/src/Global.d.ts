571e295839df14a62d248b6cecb55950338dd996
langermank <18661030+langermank@users.noreply.github.com>
2023-05-02 13:27:08 -0700

    deleting things is fun!

--- docs/src/Global.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '@primer/gatsby-theme-doctocat/src/components/clipboard-copy'
-declare module '@primer/gatsby-theme-doctocat/src/components/inline-code'
-declare module '@primer/gatsby-theme-doctocat/src/components/table'
