0d640d397318bc2521f3a1538fcfec8f12bb0d10
langermank <18661030+langermank@users.noreply.github.com>
2023-06-01 16:24:29 -0700

    fix import

--- storybook/stories/Migration/v8/TokenComparison.stories.tsx
+++ storybook/stories/Migration/v8/TokenComparison.stories.tsx
@@ -12,7 +12,7 @@ import cssShadowVars from './DeprecatedShadowTokensMap.json'
 // eslint-disable-next-line import/extensions
 import noChangeCssVars from './NoChangeTokensMap.json'
 // eslint-disable-next-line import/extensions
-import fallbackVars from '../../../../../tokens-next-private/fallbacks/color-fallbacks.json'
+import fallbackVars from '../../../../tokens-next-private/fallbacks/color-fallbacks.json'
 import {DataTable, Table} from '@primer/react/drafts'
 
 export default {
