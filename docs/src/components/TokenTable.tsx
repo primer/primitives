d7bf1048ef100ea3d448827f65b824fff1f44cd3
Katie Langerman <18661030+langermank@users.noreply.github.com>
2023-06-05 17:21:10 -0700

    Remove unused Gatsby site (#658)
    * remove gatsby site
    
    * ok fine
    
    * build script
    
    * really throwing things at the wall here
    
    * update workflows for gatsby removal
    
    ---------
    
    Co-authored-by: Lukas Oppermann <lukasoppermann@github.com>

--- docs/src/components/TokenTable.tsx
+++
@@ -1,8 +0,0 @@
-import Table from '@primer/gatsby-theme-doctocat/src/components/table'
-import styled from 'styled-components'
-
-const TokenTable = styled(Table)`
-  display: table;
-`
-
-export default TokenTable
