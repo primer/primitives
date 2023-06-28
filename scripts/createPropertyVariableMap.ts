2415d59f2c04ca811c0a2abf5738550d148c39b7
Siddharth Kshetrapal <siddharthkp@github.com>
2023-06-28 16:09:26 +0200

    add stories map

--- scripts/createPropertyVariableMap.ts
+++ scripts/createPropertyVariableMap.ts
@@ -1,6 +1,5 @@
-/**
- * breakpoints are not part of CSS.Properties
- */
+/* eslint eslint-comments/no-use: off */
+/* eslint-disable import/extensions */
 
 import fs from 'fs'
 import path from 'path'
@@ -8,6 +7,17 @@ import glob from 'glob'
 import postcss from 'postcss'
 import type {Properties} from 'csstype'
 import {flatten, uniqBy} from 'lodash'
+import {getTokensByName} from '~/docs/storybook/stories/utilities/getTokensByName'
+import type StyleDictionary from 'style-dictionary'
+
+// stories
+import functionalTypographyTokens from '~/tokens-next-private/docs/functional/typography/typography.json'
+import baseTypographyTokens from '~/tokens-next-private/docs/base/typography/typography.json'
+import functionalBorderTokens from '~/tokens-next-private/docs/functional/size/border.json'
+import functionalSizeTokens from '~/tokens-next-private/docs/functional/size/size.json'
+import functionalSizeFineTokens from '~/tokens-next-private/docs/functional/size/size-fine.json'
+import baseSizeTokens from '~/tokens-next-private/docs/base/size/size.json'
+import functionalColorTokens from '~/tokens-next-private/docs/functional/themes/light.json'
 
 // path relative to script
 const tokensDirectory = path.join(__dirname, '../tokens-next-private')
@@ -279,10 +289,188 @@ const aliases: Aliases = {
   caretColor: 'color',
 }
 
+const getTokenName = (token: Record<string, StyleDictionary.TransformedToken> | Variable) => {
+  return token.name
+}
+
+// painfully manually copied from stories
+const stories = {
+  Color: {
+    Base: {
+      Scales: {
+        AllScales: variables.filter(({file, name}) => file.name === 'light' && name.includes('color-scale')),
+      },
+    },
+    Functional: {
+      Background: [
+        'borderColor-default',
+        'borderColor-muted',
+        'borderColor-emphasis',
+        'borderColor-disabled',
+        'borderColor-transparent',
+      ],
+      Border: ['bgColor-default', 'bgColor-muted', 'bgColor-disabled', 'bgColor-emphasis', 'bgColor-transparent'],
+      Foreground: ['fgColor-default', 'fgColor-muted', 'fgColor-onEmphasis', 'fgColor-disabled', 'fgColor-link-rest'],
+      Roles: {
+        Neutral: [
+          'fgColor-neutral',
+          'bgColor-neutral-muted',
+          'bgColor-neutral-emphasis',
+          'borderColor-neutral-muted',
+          'borderColor-neutral-emphasis',
+        ],
+        Accent: [
+          'fgColor-accent',
+          'bgColor-accent-muted',
+          'bgColor-accent-emphasis',
+          'borderColor-accent-muted',
+          'borderColor-accent-emphasis',
+        ],
+        Success: [
+          'fgColor-success',
+          'bgColor-success-muted',
+          'bgColor-success-emphasis',
+          'borderColor-success-muted',
+          'borderColor-success-emphasis',
+        ],
+        Attention: [
+          'fgColor-attention',
+          'bgColor-attention-muted',
+          'bgColor-attention-emphasis',
+          'borderColor-attention-muted',
+          'borderColor-attention-emphasis',
+        ],
+        Severe: [
+          'fgColor-severe',
+          'bgColor-severe-muted',
+          'bgColor-severe-emphasis',
+          'borderColor-severe-muted',
+          'borderColor-severe-emphasis',
+        ],
+        Danger: [
+          'fgColor-danger',
+          'bgColor-danger-muted',
+          'bgColor-danger-emphasis',
+          'borderColor-danger-muted',
+          'borderColor-danger-emphasis',
+        ],
+        Open: [
+          'fgColor-open',
+          'bgColor-open-muted',
+          'bgColor-open-emphasis',
+          'borderColor-open-muted',
+          'borderColor-open-emphasis',
+        ],
+        Closed: [
+          'fgColor-closed',
+          'bgColor-closed-muted',
+          'bgColor-closed-emphasis',
+          'borderColor-closed-muted',
+          'borderColor-closed-emphasis',
+        ],
+        Done: [
+          'fgColor-done',
+          'bgColor-done-muted',
+          'bgColor-done-emphasis',
+          'borderColor-done-muted',
+          'borderColor-done-emphasis',
+        ],
+        Sponsors: [
+          'fgColor-sponsors',
+          'bgColor-sponsors-muted',
+          'bgColor-sponsors-emphasis',
+          'borderColor-sponsors-muted',
+          'borderColor-sponsors-emphasis',
+        ],
+      },
+      Shadows: [
+        'shadow-resting-xsmall',
+        'shadow-resting-small',
+        'shadow-resting-medium',
+        'shadow-floating-small',
+        'shadow-floating-medium',
+        'shadow-floating-large',
+        'shadow-highlight',
+        'shadow-inset',
+      ],
+    },
+    Patterns: {
+      Avatar: getTokensByName(functionalColorTokens, 'avatar').map(getTokenName),
+      Button: [
+        ...getTokensByName(functionalColorTokens, 'button'),
+        ...getTokensByName(functionalColorTokens, 'buttonCounter'),
+      ].map(getTokenName),
+      Control: [
+        ...getTokensByName(functionalColorTokens, 'control'),
+        ...getTokensByName(functionalColorTokens, 'controlKnob'),
+        ...getTokensByName(functionalColorTokens, 'controlTrack'),
+      ].map(getTokenName),
+      Counter: getTokensByName(functionalColorTokens, 'counter').map(getTokenName),
+      Focus: getTokensByName(functionalColorTokens, 'focus-outlineColor').map(getTokenName),
+      Header: getTokensByName(functionalColorTokens, 'header').map(getTokenName),
+      Menu: getTokensByName(functionalColorTokens, 'menu').map(getTokenName),
+      Overlay: getTokensByName(functionalColorTokens, 'overlay').map(getTokenName),
+      SelectMenu: getTokensByName(functionalColorTokens, 'selectMenu').map(getTokenName),
+      SideNav: getTokensByName(functionalColorTokens, 'sideNav').map(getTokenName),
+      TimelineBadge: getTokensByName(functionalColorTokens, 'timelineBadge').map(getTokenName),
+      UnderlineNav: getTokensByName(functionalColorTokens, 'underlineNav').map(getTokenName),
+    },
+  },
+  Typography: {
+    Base: getTokensByName(baseTypographyTokens, 'base').map(getTokenName),
+    Functional: {
+      FontFamily: getTokensByName(functionalTypographyTokens, 'fontStack').map(getTokenName),
+      FontShorthand: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+      Display: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+      TitleLarge: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+      TitleMedium: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+      TitleSmall: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+      Subtitle: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+      BodyLarge: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+      BodyMedium: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+      BodySmall: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+      Caption: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+      CodeBlock: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+      InlineCodeBlock: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+      Overview: getTokensByName(functionalTypographyTokens, 'text').map(getTokenName),
+    },
+  },
+  Size: {
+    Border: {
+      BorderSize: [
+        ...getTokensByName(functionalBorderTokens, 'boxShadow'),
+        ...getTokensByName(functionalBorderTokens, 'borderWidth'),
+      ].map(getTokenName),
+      BorderRadius: getTokensByName(functionalBorderTokens, 'borderRadius').map(getTokenName),
+      Outline: getTokensByName(functionalBorderTokens, 'outline').map(getTokenName),
+    },
+    BreakPoints: {},
+    Control: {
+      XSmall: getTokensByName(functionalSizeTokens, 'control-xsmall').map(getTokenName),
+      Small: getTokensByName(functionalSizeTokens, 'control-small').map(getTokenName),
+      Medium: getTokensByName(functionalSizeTokens, 'control-medium').map(getTokenName),
+      Large: getTokensByName(functionalSizeTokens, 'control-large').map(getTokenName),
+      XLarge: getTokensByName(functionalSizeTokens, 'control-xlarge').map(getTokenName),
+      ControlStackRegular: getTokensByName(functionalSizeTokens, 'controlStack').map(getTokenName),
+      ControlStackResponsive: getTokensByName(functionalSizeFineTokens, 'controlStack').map(getTokenName),
+      ControlTouchTarget: getTokensByName(functionalSizeTokens, 'control-minTarget').map(getTokenName),
+      ControlTouchTargetResponsive: getTokensByName(functionalSizeFineTokens, 'control-minTarget').map(getTokenName),
+    },
+    Space: {
+      Base: getTokensByName(baseSizeTokens, 'base').map(getTokenName),
+    },
+    Stack: {
+      Stack: getTokensByName(functionalSizeTokens, 'stack').map(getTokenName),
+    },
+    ViewPort: {},
+  },
+}
+
 const contents = `
   module.exports = {
     properties: ${JSON.stringify(propertyMap, null, 2)},
-    aliases: ${JSON.stringify(aliases, null, 2)}
+    aliases: ${JSON.stringify(aliases, null, 2)},
+    stories: ${JSON.stringify(stories, null, 2)}
   }
 `
 fs.writeFileSync(path.join(tokensDirectory, 'js/intellisense.js'), contents)
