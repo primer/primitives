1d803391064937e31ea32473178aabe16ad398c7
langermank <18661030+langermank@users.noreply.github.com>
2023-07-04 10:02:51 -0700

    new disabled control design

--- docs/storybook/stories/Demos/Control.stories.tsx
+++ docs/storybook/stories/Demos/Control.stories.tsx
@@ -45,7 +45,7 @@ export const ActionListScheme = () => {
       <ActionList>
         <ActionList.Item>New file</ActionList.Item>
         <ActionList.Item>Copy link</ActionList.Item>
-        <ActionList.Item>Edit file</ActionList.Item>
+        <ActionList.Item disabled>Edit file</ActionList.Item>
         <ActionList.Divider />
         <ActionList.Item variant="danger">Delete file</ActionList.Item>
       </ActionList>
@@ -196,6 +196,50 @@ export const SelectScheme = () => {
         <Select.Option value="one">Choice one</Select.Option>
         <Select.Option value="two">Choice two</Select.Option>
       </Select>
+    </div>
+  )
+}
+
+export const DisabledControls = () => {
+  const count = 12
+  return (
+    <div className="Button" style={{display: 'flex', gap: '1rem', flexDirection: 'column', alignItems: 'start'}}>
+      <Select disabled>
+        <Select.Option value="one">Choice one</Select.Option>
+        <Select.Option value="two">Choice two</Select.Option>
+      </Select>
+      <TextInput disabled aria-label="Zipcode" name="zipcode" placeholder="Zipcode" autoComplete="postal-code" />
+      <Button variant="outline" id="outline" disabled leadingIcon={HeartIcon} trailingIcon={EyeIcon}>
+        Outline
+      </Button>
+      <Button variant="danger" id="danger" disabled leadingIcon={HeartIcon} trailingIcon={EyeIcon}>
+        Danger
+      </Button>
+      <Button disabled variant="danger" id="danger">
+        Danger
+        <Button.Counter>{count}</Button.Counter>
+      </Button>
+      <Button variant="primary" id="primary" disabled leadingIcon={HeartIcon} trailingIcon={EyeIcon}>
+        Primary
+      </Button>
+      <Button disabled variant="primary" id="primary">
+        Primary
+        <Button.Counter>{count}</Button.Counter>
+      </Button>
+      <Button id="invisible" disabled variant="invisible" leadingIcon={HeartIcon} trailingIcon={EyeIcon}>
+        Invisible
+      </Button>
+      <Button disabled id="invisible" variant="invisible">
+        Invisible
+        <Button.Counter>{count}</Button.Counter>
+      </Button>
+      <Button disabled id="default">
+        Default
+        <Button.Counter>{count}</Button.Counter>
+      </Button>
+      <Button disabled id="default" leadingIcon={HeartIcon} trailingIcon={EyeIcon}>
+        Default
+      </Button>
     </div>
   )
 }
