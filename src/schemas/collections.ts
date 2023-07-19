db9b814fc60ffd23642624ef9bb117c964338788
Lukas Oppermann <lukasoppermann@github.com>
2023-07-19 18:51:03 +0200

    fix (#697)

--- src/schemas/collections.ts
+++ src/schemas/collections.ts
@@ -1,17 +1,20 @@
 import {z} from 'zod'
 import {joinFriendly} from '../utilities/joinFriendly'
 
-export const colorCollections = z.enum(['base/color/light', 'base/color/dark', 'mode', 'pattern/mode'])
-
-export const dimensionCollections = z.enum(['base/size', 'functional/size'])
-
-type Collections = 'base/color/light' | 'base/color/dark' | 'mode' | 'pattern/mode' | 'base/size' | 'functional/size'
+type Collections =
+  | 'base/color/light'
+  | 'base/color/dark'
+  | 'base/color/dark-dimmed'
+  | 'mode'
+  | 'pattern/mode'
+  | 'base/size'
+  | 'functional/size'
 
 export const collection = (collections: Collections[]) => {
   return z.string().refine(
     value => collections.includes(value as Collections),
     value => ({
-      message: `Invalid mode: "${value}", valid modes are ${joinFriendly(collections)}`,
+      message: `Invalid collection: "${value}", valid collections are ${joinFriendly(collections)}`,
     }),
   )
 }
