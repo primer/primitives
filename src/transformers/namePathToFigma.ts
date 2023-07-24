3b17a4ae14b24bf760f38b01c29d4702a5e7ca75
Lukas Oppermann <lukasoppermann@github.com>
2023-07-24 15:55:43 +0200

    fix references

--- src/transformers/namePathToFigma.ts
+++ src/transformers/namePathToFigma.ts
@@ -1,5 +1,21 @@
 import type StyleDictionary from 'style-dictionary'
 
+export const transformNamePathToFigma = (
+  token: StyleDictionary.TransformedToken,
+  options?: StyleDictionary.Platform,
+): string => {
+  let pathArray = token.path
+  if (['fgColor', 'borderColor', 'bgColor'].includes(pathArray[0]) && pathArray.length === 3) {
+    pathArray = [pathArray[0], `${pathArray[1]}-${pathArray[2]}`]
+  }
+  return (
+    [options?.prefix, ...pathArray]
+      // remove undefined if exists
+      .filter((part: unknown): part is string => typeof part === 'string')
+      .join('/')
+  )
+}
+
 /**
  * @description converts the [TransformedToken's](https://github.com/amzn/style-dictionary/blob/main/types/TransformedToken.d.ts) `.path` array to a specific figma name
  * @type name transformer — [StyleDictionary.NameTransform](https://github.com/amzn/style-dictionary/blob/main/types/Transform.d.ts)
@@ -8,16 +24,5 @@ import type StyleDictionary from 'style-dictionary'
  */
 export const namePathToFigma: StyleDictionary.Transform = {
   type: `name`,
-  transformer: (token: StyleDictionary.TransformedToken, options?: StyleDictionary.Platform): string => {
-    let pathArray = token.path
-    if (['fgColor', 'borderColor', 'bgColor'].includes(pathArray[0]) && pathArray.length === 3) {
-      pathArray = [pathArray[0], `${pathArray[1]}-${pathArray[2]}`]
-    }
-    return (
-      [options?.prefix, ...pathArray]
-        // remove undefined if exists
-        .filter((part: unknown): part is string => typeof part === 'string')
-        .join('/')
-    )
-  },
+  transformer: transformNamePathToFigma,
 }
