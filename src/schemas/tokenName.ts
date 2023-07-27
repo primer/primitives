59461002dc13c8fa32cd4d8bbc9048fba9eb5a3e
Lukas Oppermann <lukasoppermann@github.com>
2023-07-27 11:43:47 +0200

    allow @-hack

--- src/schemas/tokenName.ts
+++ src/schemas/tokenName.ts
@@ -2,7 +2,7 @@ import {z} from 'zod'
 import {schemaErrorMessage} from '../utilities/schemaErrorMessage'
 
 export const tokenName = z.string().refine(
-  name => /^[a-z0-9][A-Za-z0-9-]*$/.test(name),
+  name => /(^[a-z0-9][A-Za-z0-9-]*$|^@$)/.test(name),
   name => ({
     message: schemaErrorMessage(
       `Invalid token name: "${name}"`,
