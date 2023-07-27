2f5de35f268c7cbf0f0bec25d46386c387d52a33
Lukas Oppermann <lukasoppermann@github.com>
2023-07-27 11:07:23 +0200

    update figma name transformer and test

--- src/transformers/namePathToFigma.ts
+++ src/transformers/namePathToFigma.ts
@@ -4,14 +4,14 @@ export const transformNamePathToFigma = (
   token: StyleDictionary.TransformedToken,
   options?: StyleDictionary.Platform,
 ): string => {
-  let pathArray = token.path
+  let pathArray = token.path.filter((part: unknown): part is string => part !== '@')
   if (['fgColor', 'borderColor', 'bgColor'].includes(pathArray[0]) && pathArray.length === 3) {
     pathArray = [pathArray[0], `${pathArray[1]}-${pathArray[2]}`]
   }
   return (
     [options?.prefix, ...pathArray]
       // remove undefined if exists
-      .filter((part: unknown): part is string => typeof part === 'string')
+      .filter((part: unknown): part is string => typeof part === 'string' && part !== '@')
       .join('/')
   )
 }
