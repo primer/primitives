01aae86812435d2a6dc387d75641aa2a526cbdad
Lukas Oppermann <lukasoppermann@github.com>
2023-08-29 15:43:34 +0200

    fix shadows (#721)

---
+++ .changeset/wild-mangos-deny.md
@@ -0,0 +1,5 @@
+---
+'@primer/primitives': patch
+---
+
+Fixing shadows
--- docs/storybook/stories/Color/Functional/Shadows.stories.tsx
+++ docs/storybook/stories/Color/Functional/Shadows.stories.tsx
@@ -17,8 +17,7 @@ const shadows = [
   'shadow-floating-small',
   'shadow-floating-medium',
   'shadow-floating-large',
-  'shadow-highlight',
-  'shadow-inset',
+  'shadow-floating-xlarge',
 ]
 
 export const Shadows = () => {
@@ -27,6 +26,23 @@ export const Shadows = () => {
       {shadows.map(shadow => (
         <ColorPreview color={shadow} shadow key={shadow} canvasColor="bgColor-default" shadowBg="bgColor-default" />
       ))}
+
+      <ColorPreview
+        color="shadow-inset"
+        shadow
+        key="shadow-inset"
+        canvasColor="bgColor-muted"
+        shadowBg="bgColor-default"
+        style={{border: `1px solid var(--borderColor-default)`}}
+      />
+      <ColorPreview
+        color="shadow-highlight"
+        shadow
+        key="shadow-highlight"
+        canvasColor="bgColor-default"
+        shadowBg="bgColor-accent-emphasis"
+        style={{border: `1px solid var(--borderColor-muted)`}}
+      />
     </>
   )
 }
