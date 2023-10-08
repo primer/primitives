068dca00505fffe0714d524ec41776ff32ee3faf
Lukas Oppermann <lukasoppermann@github.com>
2023-10-08 18:11:49 +0200

    borderValue and borderToken schema tests (#757)

--- src/schemas/borderToken.ts
+++ src/schemas/borderToken.ts
@@ -5,7 +5,7 @@ import {colorHexValue} from './colorHexValue'
 import {dimensionValue} from './dimensionValue'
 import {tokenType} from './tokenType'
 
-const borderValue = z.object({
+export const borderValue = z.object({
   color: z.union([colorHexValue, referenceValue]),
   style: z.enum(['solid', 'dashed', 'dotted', 'double', 'groove', 'ridge', 'outset', 'inset']),
   width: z.union([dimensionValue, referenceValue]),
