36ed354ccfee6b7e15d529d22a0f5e3829ed8ebe
Lukas Oppermann <lukasoppermann@github.com>
2023-10-10 13:42:41 +0200

    Add test for referenceValue schema

--- src/schemas/referenceValue.ts
+++ src/schemas/referenceValue.ts
@@ -2,7 +2,7 @@ import {z} from 'zod'
 import {schemaErrorMessage} from '../utilities/schemaErrorMessage'
 
 export const referenceValue = z.string().refine(
-  ref => /^{\w+\.(\w+\.|@\.)*(\w+|@)}$/.test(ref),
+  ref => /^{\w+(\.\w+)*(\.\w+|\.@)}$/.test(ref),
   ref => ({
     message: schemaErrorMessage(
       `Invalid reference: "${ref}"`,
