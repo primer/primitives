a7045d78719f0e7612bb4d128e85850735ac3f33
Lukas Oppermann <lukasoppermann@github.com>
2023-10-16 11:34:56 +0200

    font token schema test

--- src/schemas/fontWeightValue.ts
+++ src/schemas/fontWeightValue.ts
@@ -2,6 +2,7 @@ import {z} from 'zod'
 import {schemaErrorMessage} from '../utilities/schemaErrorMessage'
 
 const allowed = [100, 200, 300, 400, 500, 600, 700, 800, 900, 950]
+
 export const fontWeightValue = z.number().refine(
   value => allowed.includes(value),
   value => ({
