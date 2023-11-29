45ba1ca3530a3ccae905fcb37314fcf26045c6da
Siddharth Kshetrapal <siddharthkp@github.com>
2023-11-29 17:23:08 +0100

    add dist files to git

---
+++ dist/js/typography/index.d.ts
@@ -0,0 +1,11 @@
+declare const _default: {
+    normal: {
+        fontSize: string[];
+        lineHeight: {
+            condensedUltra: number;
+            condensed: number;
+            default: number;
+        };
+    };
+};
+export default _default;
---
+++ dist/ts/spacing/normal.ts
@@ -0,0 +1,11 @@
+export default {
+  "spacer": [
+    "0",
+    "4px",
+    "8px",
+    "16px",
+    "24px",
+    "32px",
+    "40px"
+  ]
+}
