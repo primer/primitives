0df1e3a90138953eafe71b7e81175897b6fc3d65
Siddharth Kshetrapal <siddharthkp@github.com>
2023-11-29 22:24:29 +0100

    marketing good

--- data/colors/vars/marketing_light.ts
+++ data/colors/vars/marketing_light.ts
@@ -1,12 +1,12 @@
 export default {
   mktg: {
     btn: {
-      bg: '#1b1f23',
+      bg: "var(--control-bgColor-rest, var(--color-btn-bg, #1b1f23))",
       shadow: {
-        outline: 'rgb(0 0 0 / 15%) 0 0 0 1px inset',
-        focus: 'rgb(0 0 0 / 15%) 0 0 0 4px',
-        hover: '0 3px 2px rgba(0, 0, 0, 0.07), 0 7px 5px rgba(0, 0, 0, 0.04), 0 12px 10px rgba(0, 0, 0, 0.03), 0 22px 18px rgba(0, 0, 0, 0.03), 0 42px 33px rgba(0, 0, 0, 0.02), 0 100px 80px rgba(0, 0, 0, 0.02)',
-        hoverMuted: 'rgb(0 0 0 / 70%) 0 0 0 2px inset'
+        outline: "var(undefined, var(--color-shadow-outline, rgb(0 0 0 / 15%) 0 0 0 1px inset))",
+        focus: "var(undefined, var(--color-shadow-focus, rgb(0 0 0 / 15%) 0 0 0 4px))",
+        hover: "var(undefined, var(--color-shadow-hover, 0 3px 2px rgba(0, 0, 0, 0.07), 0 7px 5px rgba(0, 0, 0, 0.04), 0 12px 10px rgba(0, 0, 0, 0.03), 0 22px 18px rgba(0, 0, 0, 0.03), 0 42px 33px rgba(0, 0, 0, 0.02), 0 100px 80px rgba(0, 0, 0, 0.02)))",
+        hoverMuted: "var(undefined, var(--color-shadow-hover-muted, rgb(0 0 0 / 70%) 0 0 0 2px inset))"
       },
     }
   }
