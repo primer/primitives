a9d75a2031a288fee33525a9f96c6cf7dd0117b7
Simone Vittori <simone@provenance.org>
2023-12-14 12:34:31 +0000

    `npm run build:new-tokens`

---
+++ dist/ts/colors/index.ts
@@ -0,0 +1,10 @@
+import light from './light'
+import light_high_contrast from './light_high_contrast'
+import light_colorblind from './light_colorblind'
+import light_tritanopia from './light_tritanopia'
+import dark from './dark'
+import dark_dimmed from './dark_dimmed'
+import dark_high_contrast from './dark_high_contrast'
+import dark_colorblind from './dark_colorblind'
+import dark_tritanopia from './dark_tritanopia'
+export default { light, light_high_contrast, light_colorblind, light_tritanopia, dark, dark_dimmed, dark_high_contrast, dark_colorblind, dark_tritanopia }
