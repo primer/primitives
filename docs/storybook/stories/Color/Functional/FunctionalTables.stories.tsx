34458710ca8ef3848db75309231f9df8075cab15
Katie Langerman <18661030+langermank@users.noreply.github.com>
2024-02-09 18:46:20 -0800

    Storybook docs cleanup (#801)
    * cleanup
    
    * cleanup
    
    * lock
    
    * github-actions[bot] Regenerated snapshots
    
    * try adding snapshot delay
    
    * fix theme bg color
    
    * copy from main
    
    * github-actions[bot] Regenerated snapshots
    
    * try and fix snapshots
    
    * github-actions[bot] Regenerated snapshots
    
    ---------
    
    Co-authored-by: langermank <langermank@users.noreply.github.com>

---
+++ docs/storybook/stories/Color/Functional/FunctionalTables.stories.tsx
@@ -0,0 +1,202 @@
+import React from 'react'
+// eslint-disable-next-line import/extensions
+import colorTokens from '../../../../../tokens-next-private/docs/functional/themes/light.json'
+import {ColorTokenSwatch} from '../../StorybookComponents/ColorTokenSwatch/ColorTokenSwatch'
+import {DataTable, Table} from '@primer/react/drafts'
+import {InlineCode} from '../../StorybookComponents/InlineCode/InlineCode'
+import {getTokensByName} from '../../utilities/getTokensByName'
+
+export default {
+  title: 'Color/Functional/Tables',
+  parameters: {
+    controls: {hideNoControlsWarning: true},
+    options: {
+      showPanel: false,
+    },
+    tags: ['excludeSnapshot'],
+  },
+}
+
+export const Foreground = () => {
+  const data = getTokensByName(colorTokens, 'fgColor').map(token => {
+    return {
+      id: token.name,
+      ...token,
+    }
+  })
+  return (
+    <Table.Container>
+      <Table.Title as="h1" id="pattern">
+        Foreground
+      </Table.Title>
+      <DataTable
+        aria-labelledby="pattern"
+        data={data}
+        columns={[
+          {
+            header: 'Sample',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              return <ColorTokenSwatch textColor={row.name} />
+            },
+          },
+          {
+            header: 'Token',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              return <InlineCode value={row.name} copyClipboard />
+            },
+          },
+          {
+            header: 'Output value',
+            field: 'value',
+            rowHeader: true,
+            renderCell: row => {
+              return <p>{row.value}</p>
+            },
+          },
+        ]}
+      />
+    </Table.Container>
+  )
+}
+
+export const Background = () => {
+  const data = getTokensByName(colorTokens, 'bgColor').map(token => {
+    return {
+      id: token.name,
+      ...token,
+    }
+  })
+  return (
+    <Table.Container>
+      <Table.Title as="h1" id="pattern">
+        Background
+      </Table.Title>
+      <DataTable
+        aria-labelledby="pattern"
+        data={data}
+        columns={[
+          {
+            header: 'Sample',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              return <ColorTokenSwatch bgColor={row.name} />
+            },
+          },
+          {
+            header: 'Token',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              return <InlineCode value={row.name} copyClipboard />
+            },
+          },
+          {
+            header: 'Output value',
+            field: 'value',
+            rowHeader: true,
+            renderCell: row => {
+              return <p>{row.value}</p>
+            },
+          },
+        ]}
+      />
+    </Table.Container>
+  )
+}
+
+export const Border = () => {
+  const data = getTokensByName(colorTokens, 'borderColor').map(token => {
+    return {
+      id: token.name,
+      ...token,
+    }
+  })
+  return (
+    <Table.Container>
+      <Table.Title as="h1" id="pattern">
+        Border
+      </Table.Title>
+      <DataTable
+        aria-labelledby="pattern"
+        data={data}
+        columns={[
+          {
+            header: 'Sample',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              return <ColorTokenSwatch borderColor={row.name} />
+            },
+          },
+          {
+            header: 'Token',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              return <InlineCode value={row.name} copyClipboard />
+            },
+          },
+          {
+            header: 'Output value',
+            field: 'value',
+            rowHeader: true,
+            renderCell: row => {
+              return <p>{row.value}</p>
+            },
+          },
+        ]}
+      />
+    </Table.Container>
+  )
+}
+
+export const Shadow = () => {
+  const data = getTokensByName(colorTokens, 'shadow').map(token => {
+    return {
+      id: token.name,
+      ...token,
+    }
+  })
+  return (
+    <Table.Container>
+      <Table.Title as="h1" id="pattern">
+        Shadow
+      </Table.Title>
+      <DataTable
+        aria-labelledby="pattern"
+        data={data}
+        columns={[
+          {
+            header: 'Sample',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              return <ColorTokenSwatch shadowColor={row.name} />
+            },
+          },
+          {
+            header: 'Token',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              return <InlineCode value={row.name} copyClipboard />
+            },
+          },
+          {
+            header: 'Output value',
+            field: 'value',
+            rowHeader: true,
+            renderCell: row => {
+              return <p>{row.value}</p>
+            },
+          },
+        ]}
+      />
+    </Table.Container>
+  )
+}
