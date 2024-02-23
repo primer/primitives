447ce8357eaf05f165c9f0abefb24a510758566b
Lukas Oppermann <lukasoppermann@github.com>
2024-02-23 13:06:58 +0100

    adding tests

---
+++ src/transformers/utilities/isRgbaFloat.test.ts
@@ -0,0 +1,20 @@
+import {isRgbaFloat} from './isRgbaFloat'
+
+describe('Utilities: isRgbaFloat', () => {
+  it('valid RgbaFloat', () => {
+    expect(isRgbaFloat({a: 1, b: 0.6, g: 0.4, r: 0.2})).toStrictEqual(true)
+    expect(isRgbaFloat({a: 0, b: 0.2, g: 0.4, r: 0.2})).toStrictEqual(true)
+    expect(isRgbaFloat({b: 0.6, g: 0.4, r: 0.2})).toStrictEqual(true)
+  })
+
+  it('RgbaFloat as string', () => {
+    expect(isRgbaFloat('{a: 1, b: 0.6, g: 0.4, r: 0.2}')).toStrictEqual(false)
+  })
+  it('Invalid values', () => {
+    expect(isRgbaFloat({a: 1, b: 0.6, g: 0.4})).toStrictEqual(false)
+    expect(isRgbaFloat({a: 1, b: 0.6, g: 0.4, r: 1.1})).toStrictEqual(false)
+    expect(isRgbaFloat('#334455')).toStrictEqual(false)
+    expect(isRgbaFloat(undefined)).toStrictEqual(false)
+    expect(isRgbaFloat(null)).toStrictEqual(false)
+  })
+})
--- src/transformers/utilities/isRgbaFloat.ts
+++ src/transformers/utilities/isRgbaFloat.ts
@@ -5,17 +5,23 @@ export type RgbaFloat = {
   a?: number
 }
 
-// sum up the values of all values in an array
-const sum = (array: unknown[]): number => array.reduce((acc: number, v: unknown) => acc + parseInt(`${v}`), 0)
 // check if a value is an rgba float object
 export const isRgbaFloat = (value: unknown): value is RgbaFloat => {
   if (
     value &&
     typeof value === `object` &&
     'r' in value &&
+    typeof value.r === `number` &&
     'g' in value &&
+    typeof value.g === `number` &&
     'b' in value &&
-    sum([value.r, value.g, value.b]) < 5
+    typeof value.b === `number` &&
+    value.r >= 0 &&
+    value.r <= 1 &&
+    value.g >= 0 &&
+    value.g <= 1 &&
+    value.b >= 0 &&
+    value.b <= 1
   ) {
     return true
   }
