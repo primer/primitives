59a906acdc4d30c66a46da99a7393d1167f418e4
Lukas Oppermann <lukasoppermann@github.com>
2024-04-08 11:39:56 +0200

    f (#871)

--- src/formats/jsonOneDimensional.ts
+++ src/formats/jsonOneDimensional.ts
@@ -1,20 +1,16 @@
 import type StyleDictionary from 'style-dictionary'
 import {format} from 'prettier'
 import {jsonToFlat} from './utilities/jsonToFlat'
-import {prefixAllTokensArray} from './utilities/prefixAllTokensArray'
-
 /**
  * @description Takes a style dictionary token dictionary and converts it to a one dimensional json object.
  * @param StyleDictionary.FormatterArguments
  * @param options.nameSeparator - separator to use for nested token name
  * @returns formatted json `string`
  */
-export const jsonOneDimensional: StyleDictionary.Formatter = ({dictionary, file: _file, options, platform}) => {
-  // add prefix if defined
-  const allTokens = prefixAllTokensArray(dictionary.allTokens, platform)
+export const jsonOneDimensional: StyleDictionary.Formatter = ({dictionary, file: _file, options}) => {
   // option to allow verbose output (object) or just the value
   const {outputVerbose} = options
-  const tokens = jsonToFlat(allTokens, outputVerbose)
+  const tokens = jsonToFlat(dictionary.allTokens, outputVerbose)
   // add file header and convert output
   const output = JSON.stringify(tokens, null, 2)
   // return prettified
