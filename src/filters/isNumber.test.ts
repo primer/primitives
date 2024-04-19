108d61307956757675e80fb22aaf86fe72681be9
Lukas Oppermann <lukasoppermann@github.com>
2024-04-19 10:42:50 +0200

    isNumber test (#907)

---
+++ src/filters/isNumber.test.ts
@@ -0,0 +1,18 @@
+import {getMockToken} from '../test-utilities'
+import {isNumber} from './isNumber'
+
+describe('Filter: isNumber', () => {
+  it('returns true if $type property is `number`', () => {
+    expect(isNumber(getMockToken({$type: 'number'}))).toStrictEqual(true)
+  })
+
+  it('returns false if $type property is not `number`', () => {
+    expect(isNumber(getMockToken({$type: 'pumpkin'}))).toStrictEqual(false)
+  })
+
+  it('returns false if $type property is falsy', () => {
+    expect(isNumber(getMockToken({$type: false}))).toStrictEqual(false)
+    expect(isNumber(getMockToken({$type: undefined}))).toStrictEqual(false)
+    expect(isNumber(getMockToken({$type: null}))).toStrictEqual(false)
+  })
+})
