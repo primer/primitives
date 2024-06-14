672369e423ec2e092dca621061df638b166dca25
langermank <18661030+langermank@users.noreply.github.com>
2024-06-14 14:00:34 -0700

    add more dots to test

--- docs/storybook/stories/TempDotBorder.stories.tsx
+++ docs/storybook/stories/TempDotBorder.stories.tsx
@@ -9,7 +9,7 @@ export default {
 
 export const Test = () => {
   return (
-    <>
+    <div style={{display: 'inline-flex', gap: '8px'}}>
       <div
         style={{
           height: '12px',
@@ -19,6 +19,24 @@ export const Test = () => {
           border: 'solid 1px var(--borderColor-translucent)',
         }}
       ></div>
-    </>
+      <div
+        style={{
+          height: '12px',
+          width: '12px',
+          background: 'orange',
+          borderRadius: '100%',
+          border: 'solid 1px var(--borderColor-translucent)',
+        }}
+      ></div>
+      <div
+        style={{
+          height: '12px',
+          width: '12px',
+          background: 'yellow',
+          borderRadius: '100%',
+          border: 'solid 1px var(--borderColor-translucent)',
+        }}
+      ></div>
+    </div>
   )
 }
