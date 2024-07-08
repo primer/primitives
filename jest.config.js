172fb2360b51c90b60819a2095c41493b25c40ee
Lukas Oppermann <lukasoppermann@github.com>
2024-07-08 10:42:11 +0200

    working on migrating to eslint 9

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   // this is no html literal
-  // eslint-disable-next-line github/unescaped-html-literal
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {prefix: '<rootDir>/'}),
-  // eslint-disable-next-line github/unescaped-html-literal
   testPathIgnorePatterns: ['<rootDir>/e2e/'],
 }
