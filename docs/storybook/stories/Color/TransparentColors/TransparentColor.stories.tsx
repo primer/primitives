ea87d7c90a42644239121e888b13f07845522e71
Katie Langerman <18661030+langermank@users.noreply.github.com>
2024-08-08 16:09:25 -0700

    change themes

--- docs/storybook/stories/Color/TransparentColors/TransparentColor.stories.tsx
+++ docs/storybook/stories/Color/TransparentColors/TransparentColor.stories.tsx
@@ -29,9 +29,9 @@ export const Light = () => {
   })
   return (
     <Table.Container>
-      <Table.Title as="h1" id="pattern">
+      {/* <Table.Title as="h1" id="pattern">
         Light transparent colors (avoid using the global theme picker)
-      </Table.Title>
+      </Table.Title> */}
       <DataTable
         aria-labelledby="pattern"
         data={data}
@@ -82,6 +82,14 @@ export const Light = () => {
     </Table.Container>
   )
 }
+Light.decorators = [
+  (Story) => (
+    <div data-color-mode="light" data-light-theme="light" data-dark-theme="light">
+      <Story />
+    </div>
+  ),
+]
+
 
 export const Dark = () => {
   const data = Object.entries(darkColorTokens).filter(([name, token]) => token.alpha).map(([name, token]) => {
@@ -92,9 +100,9 @@ export const Dark = () => {
   })
   return (
     <Table.Container>
-      <Table.Title as="h1" id="pattern">
+      {/* <Table.Title as="h1" id="pattern">
         Light transparent colors (avoid using the global theme picker)
-      </Table.Title>
+      </Table.Title> */}
       <DataTable
         aria-labelledby="pattern"
         data={data}
@@ -145,10 +153,13 @@ export const Dark = () => {
     </Table.Container>
   )
 }
-// Dark.parameters = {
-//   theme: 'dark',
-// }
-// console.log('Dark.parameters', Dark.parameters)
+Dark.decorators = [
+  (Story) => (
+    <div data-color-mode="dark" data-light-theme="dark" data-dark-theme="dark">
+      <Story />
+    </div>
+  ),
+]
 
 export const LightHighContrast = () => {
   const data = Object.entries(lightHCColorTokens).filter(([name, token]) => token.alpha).map(([name, token]) => {
@@ -159,9 +170,9 @@ export const LightHighContrast = () => {
   })
   return (
     <Table.Container>
-      <Table.Title as="h1" id="pattern">
+      {/* <Table.Title as="h1" id="pattern">
         Light transparent colors (avoid using the global theme picker)
-      </Table.Title>
+      </Table.Title> */}
       <DataTable
         aria-labelledby="pattern"
         data={data}
@@ -212,3 +223,147 @@ export const LightHighContrast = () => {
     </Table.Container>
   )
 }
+LightHighContrast.decorators = [
+  (Story) => (
+    <div data-color-mode="light" data-light-theme="light_high_contrast" data-dark-theme="light_high_contrast">
+      <Story />
+    </div>
+  ),
+]
+
+export const DarkHighContrast = () => {
+  const data = Object.entries(darkHCColorTokens).filter(([name, token]) => token.alpha).map(([name, token]) => {
+    return {
+      id: name,
+      ...token,
+    }
+  })
+  return (
+    <Table.Container>
+      {/* <Table.Title as="h1" id="pattern">
+        Light transparent colors (avoid using the global theme picker)
+      </Table.Title> */}
+      <DataTable
+        aria-labelledby="pattern"
+        data={data}
+        columns={[
+          {
+            header: 'Sample',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              return <ColorTokenSwatch bgColor={row.name} />
+            },
+          },
+          {
+            header: 'Token',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              return <InlineCode value={row.name} copyClipboard />
+            },
+          },
+          {
+            header: 'Base color',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              const cleanedValue = `${row.original.value.replace(/[{}]/g, '').replace(/\./g, '-')}`
+              return <Stack direction="horizontal" gap="condensed"><ColorTokenSwatch bgColor={cleanedValue} /> <code>{cleanedValue}</code></Stack>
+            },
+          },
+          {
+            header: 'Alpha',
+            field: 'value',
+            rowHeader: true,
+            renderCell: row => {
+              return <p>{row.alpha}</p>
+            },
+          },
+          {
+            header: 'Output value',
+            field: 'value',
+            rowHeader: true,
+            renderCell: row => {
+              return <InlineCode value={row.value} copyClipboard />
+            },
+          },
+        ]}
+      />
+    </Table.Container>
+  )
+}
+DarkHighContrast.decorators = [
+  (Story) => (
+    <div data-color-mode="dark" data-light-theme="dark_high_contrast" data-dark-theme="dark_high_contrast">
+      <Story />
+    </div>
+  ),
+]
+
+export const DarkDimmed = () => {
+  const data = Object.entries(darkDimmedColorTokens).filter(([name, token]) => token.alpha).map(([name, token]) => {
+    return {
+      id: name,
+      ...token,
+    }
+  })
+  return (
+    <Table.Container>
+      <DataTable
+        aria-labelledby="pattern"
+        data={data}
+        columns={[
+          {
+            header: 'Sample',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              return <ColorTokenSwatch bgColor={row.name} />
+            },
+          },
+          {
+            header: 'Token',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              return <InlineCode value={row.name} copyClipboard />
+            },
+          },
+          {
+            header: 'Base color',
+            field: 'name',
+            rowHeader: true,
+            renderCell: row => {
+              const cleanedValue = `${row.original.value.replace(/[{}]/g, '').replace(/\./g, '-')}`
+              return <Stack direction="horizontal" gap="condensed"><ColorTokenSwatch bgColor={cleanedValue} /> <code>{cleanedValue}</code></Stack>
+            },
+          },
+          {
+            header: 'Alpha',
+            field: 'value',
+            rowHeader: true,
+            renderCell: row => {
+              return <p>{row.alpha}</p>
+            },
+          },
+          {
+            header: 'Output value',
+            field: 'value',
+            rowHeader: true,
+            renderCell: row => {
+              return <InlineCode value={row.value} copyClipboard />
+            },
+          },
+        ]}
+      />
+    </Table.Container>
+  )
+}
+DarkDimmed.decorators = [
+  (Story) => (
+    <div data-color-mode="dark" data-light-theme="dark_dimmed" data-dark-theme="dark_dimmed">
+      <Story />
+    </div>
+  ),
+]
