94327e972d43259316070f25ccda43820d56fae5
Lukas Oppermann <lukasoppermann@github.com>
2024-08-29 19:04:48 +0200

    extracting function

--- src/utilities/toPascalCase.ts
+++ src/utilities/toPascalCase.ts
@@ -1,22 +1,13 @@
+import {filterStringArray} from './filterStringArray'
 import {upperCaseFirstCharacter} from './upperCaseFirstCharacter'
 
 export const toPascalCase = (string: string | string[]) => {
   if (!Array.isArray(string)) {
     string = [string]
   }
-  // match unsupported characters
-  const regex = /[^a-zA-Z0-9]+/g
-  // replace any non-letter and non-number character and split into word array
-  const stringArray = string
-    .filter(part => part !== '@')
-    .filter(Boolean)
-    .join(' ')
-    .replace(regex, ' ')
-    .split(' ')
+
   return (
-    stringArray
-      // remove undefined if exists
-      .filter((part: unknown): part is string => typeof part === 'string')
+    filterStringArray(string)
       // ucFirst all but first part
       .map((part: string) => {
         return upperCaseFirstCharacter(part)
