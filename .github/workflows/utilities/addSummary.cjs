694b24ef473a05061cd32995b761364f9d0d24b2
Lukas Oppermann <lukasoppermann@github.com>
2024-11-28 13:55:07 +0100

    diff fallback config (#1097)

--- .github/workflows/utilities/addSummary.cjs
+++ .github/workflows/utilities/addSummary.cjs
@@ -7,14 +7,19 @@ module.exports = (content, overwrite = false) => {
   // core.summary.clear()
 
   for (const {title, body, sections} of content) {
+    // if no body or sections, skip
+    if(!body && (!sections || sections.length === 0)) {
+      continue
+    }
+    // add heading
     if(title) {
       core.summary.addHeading(title, '1')
     }
-
+    // add body
     if(body) {
       core.summary.addRaw(body, true)
     }
-
+    // add sections
     if(sections) {
       sections.forEach(({title, body}) => {
         const section = `<details><summary><h3>${title}</h3></summary>\n\n`+
