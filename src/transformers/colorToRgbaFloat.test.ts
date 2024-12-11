bd89c37b0ab1689e34cba305efd987e5a1470af1
Lukas Oppermann <lukasoppermann@github.com>
2024-12-11 09:26:22 +0100

    removing mix

--- src/transformers/colorToRgbaFloat.test.ts
+++ src/transformers/colorToRgbaFloat.test.ts
@@ -1,6 +1,5 @@
 import {getMockToken} from '../test-utilities/index.js'
 import {colorToRgbaFloat} from './colorToRgbaFloat.js'
-import {rgbaFloatToHex} from './utilities/rgbaFloatToHex.js'
 
 describe('Transformer: colorToRgbaFloat', () => {
   it('transforms `hex3`, `hex6`, and `hex8` tokens to rgb float value', () => {
@@ -85,53 +84,6 @@ describe('Transformer: colorToRgbaFloat', () => {
         a: 0.4,
       },
     ])
-  })
-
-  it('transforms `color` tokens including mix', () => {
-    expect(
-      [
-        getMockToken({name: 'Mix of reds', $value: '#a40e26', mix: {color: '#660018', weight: 0.4}}),
-        getMockToken({$value: '#34343466', mix: {color: '#000000'}}),
-        getMockToken({$value: 'rgb(100,200,255)', mix: {weight: 0.5}}),
-        getMockToken({$value: 'rgba(100,200,255,0.8)', mix: {color: undefined, weight: undefined}}),
-      ].map(item => colorToRgbaFloat.transform(item, {}, {})),
-    ).toStrictEqual([
-      {
-        b: 0.12549019607843137,
-        g: 0.03137254901960784,
-        r: 0.5450980392156862,
-        a: 1,
-      },
-      {
-        r: 0.20392156862745098,
-        g: 0.20392156862745098,
-        b: 0.20392156862745098,
-        a: 0.4,
-      },
-      {
-        r: 0.39215686274509803,
-        g: 0.7843137254901961,
-        b: 1,
-        a: 1,
-      },
-      {
-        r: 0.39215686274509803,
-        g: 0.7843137254901961,
-        b: 1,
-        a: 0.8,
-      },
-    ])
-
-    expect(
-      [
-        getMockToken({name: 'Mix of reds', $value: '#a40e26', mix: {color: '#660018', weight: 0.4}}),
-        getMockToken({$value: '#34343466', mix: {color: '#000000'}}),
-        getMockToken({$value: 'rgb(100,200,255)', mix: {weight: 0.5}}),
-        getMockToken({$value: 'rgba(100,200,255,0.8)', mix: {color: undefined, weight: undefined}}),
-      ]
-        .map(item => colorToRgbaFloat.transform(item, {}, {}) as {r: number; g: number; b: number; a?: number})
-        .map(item => rgbaFloatToHex(item)),
-    ).toStrictEqual(['#8b0820', '#34343466', '#64c8ff', '#64c8ffcc'])
   })
 
   it('it forwards `rgb float` values', () => {
