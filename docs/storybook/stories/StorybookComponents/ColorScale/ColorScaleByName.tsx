eb2663a5f22d87beceaa4feede794544f1e06ff8
Lukas Oppermann <lukasoppermann@github.com>
2025-05-27 14:20:33 +0200

    fix formattingh

--- docs/storybook/stories/StorybookComponents/ColorScale/ColorScaleByName.tsx
+++ docs/storybook/stories/StorybookComponents/ColorScale/ColorScaleByName.tsx
@@ -7,7 +7,7 @@ export type ColorScaleProps = {
   steps: number
 }
 
-const ColorBlock = ({color}: {color: string;}) => {
+const ColorBlock = ({color}: {color: string}) => {
   const ref = React.useRef<HTMLDivElement | null>(null)
   const [hex, setHex] = React.useState<string | null>(null)
   const [textColor, setTextColor] = React.useState<string>('currentColor')
@@ -25,7 +25,8 @@ const ColorBlock = ({color}: {color: string;}) => {
     }, 0)
   }, [color])
 
-  return (<div
+  return (
+    <div
       className="ColorScale--block"
       style={{
         backgroundColor: `var(--${color})`,
@@ -33,23 +34,24 @@ const ColorBlock = ({color}: {color: string;}) => {
       }}
       data-color-scale
       ref={ref}
-    >      <p data-token-name>{color}</p>
+    >
+      {' '}
+      <p data-token-name>{color}</p>
       {hex ? <p data-token-name>{hex}</p> : null}
-    </div>)
+    </div>
+  )
 }
 
 export const ColorScaleByName = ({colorBaseVariable, steps}: ColorScaleProps) => {
-
   return (
     <div>
-    {new Array(steps).fill(null).map((_, index) => {
-      const color = `${colorBaseVariable}-${index}`
-      return (
-        <ColorBlock key={color} color={color} />)
-    })}
-  </div>)
+      {new Array(steps).fill(null).map((_, index) => {
+        const color = `${colorBaseVariable}-${index}`
+        return <ColorBlock key={color} color={color} />
+      })}
+    </div>
+  )
 }
-
 
 // export default ColorScale
 
