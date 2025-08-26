8644047564d4a617349fcf4097d0a63641950566
Lukas Oppermann <lukasoppermann@github.com>
2025-08-26 18:12:17 +0200

    fix ts issues

--- src/schemas/dimensionValue.ts
+++ src/schemas/dimensionValue.ts
@@ -2,15 +2,17 @@ import {z} from 'zod'
 import {schemaErrorMessage} from '../utilities/index.js'
 
 export const dimensionValue = z.union([
-  z.string().refine(
-    dim => /(^-?[0-9]+(px|rem)$|^-?[0-9]+\.?[0-9]*em$)/.test(dim),
-    val => ({
-      message: schemaErrorMessage(
-        `Invalid dimension: "${val}"`,
-        `Dimension must be a string with a unit (px, rem or em) or 0`,
-      ),
-    }),
-  ),
+  z.string().superRefine((dim, ctx) => {
+    if (!/(^-?[0-9]+(px|rem)$|^-?[0-9]+\.?[0-9]*em$)/.test(dim)) {
+      ctx.addIssue({
+        code: 'custom',
+        message: schemaErrorMessage(
+          `Invalid dimension: "${dim}"`,
+          `Dimension must be a string with a unit (px, rem or em) or 0`,
+        ),
+      })
+    }
+  }),
   z.literal('0'),
   z.literal(0),
 ])
