65fa7418e35c965f3647f16290e81979fa0bf37e
Lukas Oppermann <lukasoppermann@github.com>
2026-01-19 10:57:34 +0100

    add integration test

---
+++ integration/build/integration/js/esm.mjs
@@ -0,0 +1,14 @@
+export default {
+  PREFIX: {
+    base: {
+      color: {
+        aquaBlue: {
+          "500": "#2c29ff",
+        },
+      },
+    },
+    fgColor: {
+      "link-rest-01": "#2c29ff",
+    },
+  },
+};
