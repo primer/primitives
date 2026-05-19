ed0daa41a95ade035cbad5c7875abcb0c1ed754b
Lukas Oppermann <lukasoppermann@github.com>
2026-05-19 09:28:57 +0200

    chore: revert DisplayColors.stories.tsx to main version

--- docs/storybook/stories/Color/Display/DisplayColors.stories.tsx
+++ docs/storybook/stories/Color/Display/DisplayColors.stories.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import {Stack} from '@primer/react/experimental'
+import {DataTable, Stack, Table} from '@primer/react/experimental'
+import {ColorTokenSwatch} from '../../StorybookComponents/ColorTokenSwatch/ColorTokenSwatch'
+import {InlineCode} from '../../StorybookComponents/InlineCode/InlineCode'
+import {getTokensByName} from '../../utilities/getTokensByName'
+import {withColorTokens} from '../../utilities/withColorTokens'
+import {formatTokenValue} from '../../utilities/formatTokenValue'
 
 export default {
   title: 'Color/Display/Display Colors',
+  decorators: [withColorTokens],
   parameters: {
     controls: {hideNoControlsWarning: true},
     options: {
@@ -33,7 +39,7 @@ const colors = [
   'auburn',
 ]
 
-const labelStyle: React.CSSProperties = {
+const labelStyle = {
   display: 'inline-flex',
   alignItems: 'center',
   padding: '2px 8px',
@@ -46,7 +52,7 @@ const labelStyle: React.CSSProperties = {
   textDecoration: 'none',
 }
 
-const SectionHeading = ({children}: {children: React.ReactNode}) => (
+const SectionHeading = ({children}) => (
   <h2
     style={{
       fontSize: '14px',
@@ -177,6 +183,126 @@ export const DisplayColors = () => {
           ))}
         </div>
       </div>
+    </Stack>
+  )
+}
+
+export const Foreground = ({colorTokens}) => {
+  const data = getTokensByName(colorTokens, 'display')
+    .filter(token => token.name.includes('fgColor'))
+    .map(token => ({id: token.name, ...token}))
+
+  return (
+    <Stack direction="vertical" gap="spacious" style={{padding: '24px'}}>
+      <Table.Container>
+        <Table.Title as="h1" id="fg-heading">
+          Foreground
+        </Table.Title>
+        <DataTable
+          aria-labelledby="fg-heading"
+          data={data}
+          columns={[
+            {
+              header: 'Sample',
+              field: 'name',
+              rowHeader: true,
+              renderCell: row => <ColorTokenSwatch textColor={row.name} />,
+            },
+            {
+              header: 'Token',
+              field: 'name',
+              rowHeader: true,
+              renderCell: row => <InlineCode value={row.name} copyClipboard cssVar />,
+            },
+            {
+              header: 'Output value',
+              field: 'value',
+              rowHeader: true,
+              renderCell: row => <p>{formatTokenValue(row.value)}</p>,
+            },
+          ]}
+        />
+      </Table.Container>
+    </Stack>
+  )
+}
+
+export const Background = ({colorTokens}) => {
+  const data = getTokensByName(colorTokens, 'display')
+    .filter(token => token.name.includes('bgColor'))
+    .map(token => ({id: token.name, ...token}))
+
+  return (
+    <Stack direction="vertical" gap="spacious" style={{padding: '24px'}}>
+      <Table.Container>
+        <Table.Title as="h1" id="bg-heading">
+          Background
+        </Table.Title>
+        <DataTable
+          aria-labelledby="bg-heading"
+          data={data}
+          columns={[
+            {
+              header: 'Sample',
+              field: 'name',
+              rowHeader: true,
+              renderCell: row => <ColorTokenSwatch bgColor={row.name} />,
+            },
+            {
+              header: 'Token',
+              field: 'name',
+              rowHeader: true,
+              renderCell: row => <InlineCode value={row.name} copyClipboard cssVar />,
+            },
+            {
+              header: 'Output value',
+              field: 'value',
+              rowHeader: true,
+              renderCell: row => <p>{formatTokenValue(row.value)}</p>,
+            },
+          ]}
+        />
+      </Table.Container>
+    </Stack>
+  )
+}
+
+export const Border = ({colorTokens}) => {
+  const data = getTokensByName(colorTokens, 'display')
+    .filter(token => token.name.includes('borderColor'))
+    .map(token => ({id: token.name, ...token}))
+
+  return (
+    <Stack direction="vertical" gap="spacious" style={{padding: '24px'}}>
+      <Table.Container>
+        <Table.Title as="h1" id="border-heading">
+          Border
+        </Table.Title>
+        <DataTable
+          aria-labelledby="border-heading"
+          data={data}
+          columns={[
+            {
+              header: 'Sample',
+              field: 'name',
+              rowHeader: true,
+              renderCell: row => <ColorTokenSwatch borderColor={row.name} />,
+            },
+            {
+              header: 'Token',
+              field: 'name',
+              rowHeader: true,
+              renderCell: row => <InlineCode value={row.name} copyClipboard cssVar />,
+            },
+            {
+              header: 'Output value',
+              field: 'value',
+              rowHeader: true,
+              renderCell: row => <p>{formatTokenValue(row.value)}</p>,
+            },
+          ]}
+        />
+      </Table.Container>
     </Stack>
   )
 }
